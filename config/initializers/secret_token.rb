1c6a34945caa56ad1d924b5c1ead254b3a123554
Bodo Tasche <bodo@wannawork.de>
2011-03-27 12:27:08 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hcking::Application.config.secret_token = '1175186721f60f54ed0ac30c41306ef92c905494680aefb79a63c16c78d11e34d88c79cb6bb3132d10d882da007afc1480bc5af651eef4f601bda29b4f006ffa'
