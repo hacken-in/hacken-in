3f2e81ecf011b082bcea9fdda219c555ef34a34b
Bodo Tasche <bodo@wannawork.de>
2011-04-25 21:43:46 +0200

    update devise

--- db/migrate/20110327194043_devise_create_users.rb
+++ db/migrate/20110327194043_devise_create_users.rb
@@ -10,7 +10,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
 
-
+      t.encryptable
       t.timestamps
     end
 
