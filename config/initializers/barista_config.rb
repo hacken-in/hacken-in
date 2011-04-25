b298c295cc30733d2e0daba9f2b53da1d6516ab4
Bodo Tasche <bodo@wannawork.de>
2011-04-25 20:23:43 +0200

    changed root for coffeescripts

--- config/initializers/barista_config.rb
+++ config/initializers/barista_config.rb
@@ -2,7 +2,7 @@
 Barista.configure do |c|
   
   # Change the root to use app/scripts
-  # c.root = Rails.root.join("app", "scripts")
+  c.root = Rails.root.join("app", "coffeescripts")
   
   # Change the output root, causing Barista to compile into public/coffeescripts
   c.output_root = Rails.root.join("public", "coffeescripts")
