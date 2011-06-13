0c2cac1ffa7359f820a72f104260710172058134
Bodo Tasche <bodo@wannawork.de>
2011-06-13 15:53:55 +0200

    fixed event.schedule=

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -17,6 +17,12 @@ class EventTest < ActiveSupport::TestCase
     event.schedule_yaml = "--- \n:start_date: 2011-06-13 14:20:22 +02:00\n:rrules: []\n\n:exrules: []\n\n:rdates: \n- 2011-06-13 14:20:22 +02:00\n:exdates: []\n\n:duration: \n:end_time: \n"
     assert_equal 1, event.schedule.all_occurrences.size
     assert_equal Time.new(2011,6,13,14,20,22,"+02:00"), event.schedule.all_occurrences.first
+
+    event = Event.new
+    schedule = IceCube::Schedule.new(Time.new(2011,6,14,19,30))
+    schedule.add_recurrence_date(Time.new(2011,6,13,14,20,0,0))
+    event.schedule = schedule
+    assert_equal 1, event.schedule.all_occurrences.size
   end
 
   test "find events in range" do
