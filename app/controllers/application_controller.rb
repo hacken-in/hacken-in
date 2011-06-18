26f90ac084001cd3326e69842aac9bd72ec5d569
Bodo Tasche <bodo@wannawork.de>
2011-06-18 08:41:51 +0200

    CanCan added + tests for event edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => "Leider darfst du das nicht."
+  end
+
 end
