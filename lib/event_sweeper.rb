210c2e3236a147be70e539b34b9815046f2ff4f5
Bodo Tasche <bodo@wannawork.de>
2011-08-06 22:47:59 +0200

    Added cache_sweeper

---
+++ lib/event_sweeper.rb
@@ -0,0 +1,24 @@
+class EventSweeper < ActionController::Caching::Sweeper
+  observe Event
+
+  def after_create(event)
+    expire_cache_for(event)
+  end
+
+  # If our sweeper detects that a event was updated call this
+  def after_update(event)
+    expire_cache_for(event)
+  end
+
+  # If our sweeper detects that a event was deleted call this
+  def after_destroy(event)
+    expire_cache_for(event)
+  end
+
+  private
+
+  def expire_cache_for(event)
+    expire_fragment("event_occurences_#{event.id}")
+    expire_action(:controller => 'welcome', :action => 'index')
+  end
+end
