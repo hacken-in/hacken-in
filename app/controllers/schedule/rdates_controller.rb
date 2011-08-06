bae1f331a57b028215f21476402a5dfb81bd213c
Bodo Tasche <bodo@wannawork.de>
2011-08-06 21:44:23 +0200

    Modifying exdates and rdates done, missing tests

---
+++ app/controllers/schedule/rdates_controller.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+require 'datetime_parser'
+
+class Schedule::RdatesController < ApplicationController
+  include DatetimeParser
+
+  def create
+    @event = Event.find(params[:event_id])
+    authorize! :update, @event
+
+    rdate = parse_datetime_select(params[:rdate], "date")
+    @event.schedule.add_recurrence_date(rdate)
+    if !@event.save
+      redirect_to(@event, :alert => 'Datum konnte nicht hinzugefügt werden.')
+    else
+      redirect_to(@event, :notice => 'Datum hinzugefügt.')
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    authorize! :update, @event
+
+    rdate = @event.schedule.rdates[params[:id].to_i]
+    @event.schedule.remove_recurrence_date(rdate)
+
+    if !@event.save
+      redirect_to(@event, :alert => 'Datum konnte nicht entfernt werden.')
+    else
+      redirect_to(@event, :notice => 'Datum entfernt.')
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Hcking::Application.routes.draw do
   devise_for :users
 
-  resources :events
+  resources :events do
+    namespace "schedule" do
+      resources :rdates, :exdates
+    end
+  end
   match 'ical' => "ical#index"
 
   root :to => 'welcome#index'
