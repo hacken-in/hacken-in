d6691e70898726771694df02aacc3f2e174862e0
Bodo Tasche <bodo@wannawork.de>
2011-08-07 16:35:54 +0200

    Expire the cache in 10 minutes

--- app/controllers/ical_controller.rb
+++ app/controllers/ical_controller.rb
@@ -1,5 +1,5 @@
 class IcalController < ApplicationController
-  caches_action :index
+  caches_action :inde, :expires_in => 10.minutes
 
   def index
     response.headers["Content-Type"] = "text/calendar"
