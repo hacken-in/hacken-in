7fd4ade376eff732bf2b55bdbdbab2cce953cbfe
Bodo Tasche <bodo@wannawork.de>
2011-08-07 08:50:13 +0200

    Duration should be in minute not in seconds

--- app/models/event.rb
+++ app/models/event.rb
@@ -37,10 +37,11 @@ class Event < ActiveRecord::Base
           @schedule = IceCube::Schedule.from_yaml(self.schedule_yaml)
         rescue => e
           # Shit, parsing went wrong
-          @schedule = IceCube::Schedule.new(Time.now, :duration => 60)
         end
-      else
-        @schedule = IceCube::Schedule.new(Time.now, :duration => 60)
+      end
+
+      if @schedule.nil?
+        @schedule = IceCube::Schedule.new(Time.now, :duration => 60 * 60)
       end
     end
     @schedule
