cf8feaaf99d4cc278f428170035dc5b7d5f17e46
Bodo Tasche <bodo@wannawork.de>
2011-08-07 22:00:30 +0200

    Separated URL from description field

--- app/controllers/ical_controller.rb
+++ app/controllers/ical_controller.rb
@@ -5,13 +5,13 @@ class IcalController < ApplicationController
     response.headers["Content-Type"] = "text/calendar"
     cal = RiCal.Calendar do
       Event.get_ordered_events(Date.today, Date.today + 8.weeks).each do |entry|
-        desc = [entry[:event].description, entry[:event].url].delete_if {|d| d.nil?}
         event do
           summary     entry[:event].name
-          description ActionController::Base.helpers.strip_tags(desc.join("\n\n"))
+          description ActionController::Base.helpers.strip_tags(entry[:event].description || "")
           dtstart     entry[:time].utc
           dtend       (entry[:time] + (entry[:event].schedule.duration || 3600)).utc
           location    (entry[:event].address || "")
+          url         entry[:event].url if !entry[:event].url.blank?
         end
       end
     end
