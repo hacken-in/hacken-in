5298e8bde368092599cfdf9f0b4d879a6d7c120d
Bodo Tasche <bodo@wannawork.de>
2011-08-07 11:53:35 +0200

    Added ical to cache and removed html from ical

--- app/controllers/ical_controller.rb
+++ app/controllers/ical_controller.rb
@@ -1,12 +1,14 @@
 class IcalController < ApplicationController
+  caches_action :index
 
   def index
     response.headers["Content-Type"] = "text/calendar"
     cal = RiCal.Calendar do
       Event.get_ordered_events(Date.today, Date.today + 8.weeks).each do |entry|
+        desc = [entry[:event].description, entry[:event].url].delete_if {|d| d.nil?}
         event do
           summary     entry[:event].name
-          description (entry[:event].description || "")
+          description ActionController::Base.helpers.strip_tags(desc.join("\n\n"))
           dtstart     entry[:time].utc
           dtend       (entry[:time] + (entry[:event].schedule.duration || 3600)).utc
           location    (entry[:event].address || "")
