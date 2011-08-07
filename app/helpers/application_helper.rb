fb557a9bc03ef2b68be53c41ce4fce34314e8b95
Bodo Tasche <bodo@wannawork.de>
2011-08-07 10:41:56 +0200

    Now it's possible to add new simple rules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
 
+  def weekday_select_option
+    daynames = t("date.day_names").collect.with_index {|x,i| [x,i]}
+    daynames << daynames.delete_at(0)
+    options_for_select(daynames)
+  end
+
 end
