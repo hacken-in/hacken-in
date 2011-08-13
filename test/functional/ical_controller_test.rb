7ecd0d5498db1916769bd4ef311df82eae6a722e
Bodo Tasche <bodo@wannawork.de>
2011-08-13 17:20:29 +0200

    ical formatting changed

--- test/functional/ical_controller_test.rb
+++ test/functional/ical_controller_test.rb
@@ -15,7 +15,20 @@ class IcalControllerTest < ActionController::TestCase
     time2 = (Date.today + 3.days).to_time
     event2.schedule.add_recurrence_date(time2)
     event2.full_day = true
+    event2.location = "home"
+    event2.street = "street"
+    event2.zipcode = "zipcode"
+    event2.city = "cologne"
     event2.save
+
+    event3 = FactoryGirl.create(:simple)
+    time3 = Time.now + 24.hours
+    event3.schedule.add_recurrence_date(time3)
+    event3.full_day = true
+    event3.street = "street"
+    event3.zipcode = "zipcode"
+    event3.city = "cologne"
+    event3.save
 
     get :index
     assert_response :success
@@ -27,19 +40,25 @@ PRODID;X-RICAL-TZSOURCE=TZINFO:-//com.denhaven2/NONSGML ri_cal gem//EN
 CALSCALE:GREGORIAN
 VERSION:2.0
 BEGIN:VEVENT
+DTEND;VALUE=DATE:#{time3.strftime("%Y%m%d")}
+DTSTART;VALUE=DATE:#{time3.strftime("%Y%m%d")}
+DESCRIPTION:
+SUMMARY:SimpleEvent
+LOCATION:street\\, zipcode cologne
+END:VEVENT
+BEGIN:VEVENT
 DTEND;VALUE=DATE-TIME:#{(time + 1.hour).utc.strftime("%Y%m%dT%H%M%SZ")}
 DTSTART;VALUE=DATE-TIME:#{time.utc.strftime("%Y%m%dT%H%M%SZ")}
 DESCRIPTION:description
 URL:url
 SUMMARY:SimpleEvent
-LOCATION:\\, \\, 
 END:VEVENT
 BEGIN:VEVENT
 DTEND;VALUE=DATE:#{time2.strftime("%Y%m%d")}
 DTSTART;VALUE=DATE:#{time2.strftime("%Y%m%d")}
 DESCRIPTION:
 SUMMARY:SimpleEvent
-LOCATION:\\, \\, 
+LOCATION:home\\, street\\, zipcode cologne
 END:VEVENT
 END:VCALENDAR
 DESC
