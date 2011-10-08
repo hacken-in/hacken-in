da8f877a412092933ede1313dc24dd779031075e
Bodo Tasche <bodo@wannawork.de>
2011-10-08 20:01:50 +0200

    Clipboard-Link now with picture

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
-
   test "should get index" do
     get :show, {page_name: 'impressum'}
     assert_response :success
