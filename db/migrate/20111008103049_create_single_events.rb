d54dcd191560ca349affab522773564428f66c89
moonglum <moonglum@moonbeamlabs.com>
2011-10-08 16:55:42 +0200

    Removed Unused Database Field
    Date is unneccessary, because it is all saved in Time

--- db/migrate/20111008103049_create_single_events.rb
+++ db/migrate/20111008103049_create_single_events.rb
@@ -3,7 +3,6 @@ class CreateSingleEvents < ActiveRecord::Migration
     create_table :single_events do |t|
       t.string :topic
       t.text :description
-      t.date :date
       t.time :time
       t.integer :event_id
 
