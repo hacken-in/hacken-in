7212a45c7e5f9bba959340badb5a170b46b4e218
moonglum <moonglum@moonbeamlabs.com>
2011-10-11 13:01:27 +0200

    Todo now in Rails format (rake notes)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   def index
     @single_events = SingleEvent.getNextWeeks 4
 
-    # Todo: Remove SingleEvents that are hated
+    # TODO: Remove SingleEvents that are hated
     #if user_signed_in? && !current_user.hate_list.empty?
     #  @events = Event.tagged_with(current_user.hate_list, exclude: true).get_ordered_events(Date.today, Date.today + 4.weeks)
     #else
