c1f5634fd7a045fac7e87c2c140222527854d0ba
moonglum <moonglum@moonbeamlabs.com>
2011-10-12 22:36:59 +0200

    Sorry for that mess…

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   caches_action :index, :expires_in => 10.minutes, :if => Proc.new{ !(can?(:create, Event)) }
 
   def index
-<<<<<<< HEAD
     @single_events = SingleEvent.getNextWeeks 4
 
     # TODO: Remove SingleEvents that are hated
@@ -11,15 +10,6 @@ class WelcomeController < ApplicationController
     #else
     #  @events = Event.get_ordered_events(Date.today, Date.today + 4.weeks)
     #end
-=======
-    
-    if user_signed_in? && !current_user.hate_list.empty?
-      
-      @events = Event.tagged_with(current_user.hate_list, exclude: true).get_ordered_events(Date.today, Date.today + 4.weeks)
-    else
-      @events = Event.get_ordered_events(Date.today, Date.today + 4.weeks)
-    end
->>>>>>> master
   end
 
 end
