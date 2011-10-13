b52e67db090e52f1cc99f7b0bcc460ea9e827367
Bodo Tasche <bodo@wannawork.de>
2011-10-13 19:54:46 +0200

    Welcome-Page now shows filtered events, fixed a few tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111009121736) do
+ActiveRecord::Schema.define(:version => 20111011083925) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(:version => 20111009121736) do
   create_table "single_events", :force => true do |t|
     t.string   "topic"
     t.text     "description"
-    t.datetime "time"
     t.integer  "event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "when"
   end
 
   create_table "taggings", :force => true do |t|
