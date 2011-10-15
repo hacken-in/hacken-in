8130b398466215f0a2f8044ff396fd29beba1a6b
Bodo Tasche <bodo@wannawork.de>
2011-10-15 22:01:58 +0200

    sort events in tag list

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,7 +9,7 @@ class TagsController < ApplicationController
   end
   
   def show
-    @events = Event.tagged_with(params[:tagname])
+    @events = Event.tagged_with(params[:tagname]).order(:name)
   end
   
 end
