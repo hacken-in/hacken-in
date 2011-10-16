8ba972a00fd47f7feba4dfee2fda836692b7301d
Bodo Tasche <bodo@wannawork.de>
2011-10-16 14:18:49 +0200

    refactored a little bit. shorter is better :)

--- app/models/single_event.rb
+++ app/models/single_event.rb
@@ -3,26 +3,17 @@ class SingleEvent < ActiveRecord::Base
   scope :in_future, where("occurrence >= ?", Time.now).order(:occurrence)
   default_scope order(:occurrence)
 
-  def SingleEvent.find_or_create(parameters)
-    event = self.where(parameters).first
-
-    if event.nil?
-      return self.create parameters
-    else
-      return event
-    end
+  def self.find_or_create(parameters)
+    event = where(parameters).first
+    event.nil? ? create(parameters) : event
   end
 
-  def SingleEvent.getNextWeeks(number_of_weeks)
-    self.where(:occurrence => (Time.now.to_date)..((Time.now + number_of_weeks.weeks).to_date))
+  def self.getNextWeeks(number_of_weeks)
+    where(:occurrence => (Time.now.to_date)..((Time.now + number_of_weeks.weeks).to_date))
   end
 
   def title
-    if self.topic.blank?
-      self.event.name
-    else
-      "#{self.topic} (#{self.event.name})"
-    end
+    self.topic.blank? ? self.event.name : "#{self.topic} (#{self.event.name})"
   end
 
 end
