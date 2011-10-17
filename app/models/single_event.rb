64964dfc90a253cdc929ce8b640ec0a07d749525
moonglum <moonglum@moonbeamlabs.com>
2011-10-17 11:49:28 +0200

    First attempt to sort single_events
    Wrote all tests for checking if the agreed sorting is working.
    Two are failing (the most simple ones…) – maybe I have a code-blockade.
    Would someone look at it?

--- app/models/single_event.rb
+++ app/models/single_event.rb
@@ -9,11 +9,42 @@ class SingleEvent < ActiveRecord::Base
   end
 
   def self.getNextWeeks(number_of_weeks)
-    where(:occurrence => (Time.now.to_date)..((Time.now + number_of_weeks.weeks).to_date))
+    where(:occurrence => (Time.now.to_date)..((Time.now + number_of_weeks.weeks).to_date)).sort
   end
 
   def title
     self.topic.blank? ? self.event.name : "#{self.topic} (#{self.event.name})"
+  end
+
+  def <=>(other)
+    if (self.occurrence.year != other.occurrence.year) || (self.occurrence.month != other.occurrence.month) || (self.occurrence.day != other.occurrence.day)
+      # not on same day..,
+      return self.occurrence <=> other.occurrence
+    elsif self.event.full_day
+      if other.event.full_day
+        # both are all day
+        # sort via topic
+        return self.title <=> other.title
+      else
+        # self is all day, other is not
+        return -1
+      end
+    elsif other.event.full_day
+      # sother is all day, self is not
+      return 1
+    else
+      # both are not all day
+      # sort via time
+      time_comparison = (self.occurrence <=> other.occurrence)
+
+      if time_comparison == 0
+        # they are at the same time
+        # sort via topic
+        return self.title <=> other.title
+      else
+        return time_comparison
+      end
+    end
   end
 
 end
