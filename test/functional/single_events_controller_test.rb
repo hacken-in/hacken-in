730861e9d16cd96476e46065f4f1dc31ee17c726
moonglum <moonglum@moonbeamlabs.com>
2011-10-25 14:43:22 +0200

    Added Tests for SingleEvents Controller #37
    Also fixed two little spelling mistake

--- test/functional/single_events_controller_test.rb
+++ test/functional/single_events_controller_test.rb
@@ -1,18 +1,39 @@
+# encoding: utf-8
+
 require 'test_helper'
 
 class SingleEventsControllerTest < ActionController::TestCase
-#  test "should get show" do
-#    get :show
-#    assert_response :success
-#  end
-#
-#  test "should get edit" do
-#    get :edit
-#    assert_response :success
-#  end
-#
-#  test "should get update" do
-#    get :update
-#    assert_response :success
-#  end
+  include Devise::TestHelpers
+
+  test "should be successful" do
+    single_event = FactoryGirl.create(:single_event)
+    get :show, :id => single_event.id, :event_id => single_event.event.id
+    assert_response :success
+  end
+
+  test "should render the comments partial in order to show and post comments" do
+    single_event = FactoryGirl.create(:single_event)
+    get :show, :id => single_event.id, :event_id => single_event.event.id
+    assert_template("_comments")
+  end
+
+  test "should not be able to edit if not bodo" do
+    single_event = FactoryGirl.create(:single_event)
+    get :edit, :id => single_event.id, :event_id => single_event.event.id
+    sign_in FactoryGirl.create(:user)
+
+    assert_redirected_to :controller => 'welcome', :action => 'index'
+  end
+
+  test "should be able to edit if bodo" do
+    single_event = FactoryGirl.create(:single_event)
+    get :edit, :id => single_event.id, :event_id => single_event.event.id
+    sign_in FactoryGirl.create(:bodo)
+
+    assert_response :success
+
+    put :update, :id => single_event.id, :event_id => single_event.event.id, :single_event => {:topic => "Something new"}
+    single_event.reload
+    assert_match /Something new.+/, single_event.name
+  end
 end
