ef6cd12be943c54832431866d5f37c4858a8b834
Bodo Tasche <bodo@wannawork.de>
2011-11-27 19:02:36 +0100

    fixed handling of events, do not delete array you are working in :)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,13 +9,14 @@ class TagsController < ApplicationController
   end
   
   def show
-    @events = Event.tagged_with(params[:tagname]).order(:name).to_a
-    @single_events = SingleEvent.in_future.tagged_with(params[:tagname]).where("occurrence < ? ", Date.today + 1.month)
-    @events.each do |event|
+    @single_events = SingleEvent.in_future.where("occurrence < ? ", Date.today + 1.month).tagged_with(params[:tagname]).to_a
+    @events = []
+    Event.tagged_with(params[:tagname]).order(:name).each do |event|
       future_events = event.single_events.in_future.where("occurrence < ? ", Date.today + 1.month)
-      if !future_events.empty?
+      unless future_events.empty?
         @single_events = @single_events + future_events
-        @events.delete(event)
+      else
+        @events << event
       end
     end
     @single_events = @single_events.sort_by(&:occurrence)
