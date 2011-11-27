46cb2807515912b235e836899473427575d95dcb
Bodo Tasche <bodo@wannawork.de>
2011-11-27 17:55:39 +0100

    show single events on tag page (closes #35)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,7 +9,15 @@ class TagsController < ApplicationController
   end
   
   def show
-    @events = Event.tagged_with(params[:tagname]).order(:name)
+    @events = Event.tagged_with(params[:tagname]).order(:name).to_a
+    @single_events = SingleEvent.in_future.tagged_with(params[:tagname]).sort_by(&:occurrence).delete_if {|o| o.occurrence > Date.today + 1.month}
+    @events.each do |event|
+      future_events = event.single_events.in_future.delete_if {|o| o.occurrence > Date.today + 1.month}
+      if !future_events.empty?
+        @single_events = @single_events + future_events
+        @events.delete(event)
+      end
+    end
   end
-  
+
 end
