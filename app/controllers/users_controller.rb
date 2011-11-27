0116ce9c0357505ef5420d2be598a701c55b8f0e
Tobias Eilert <tobias.eilert@tux-mail.de>
2011-11-27 15:21:12 +0100

    The user profile now shows his recent activity (events and comments) #49

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find params[:id]
+    
+    # Collect recent activity of this user:
+    @recent_events = @user.single_events.recent
+    @recent_comments = @user.comments.recent
+    
   end
+  
 end
 
