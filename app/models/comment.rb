e81f24b6f7ce496701c7bdde17a2ed42fd5377f4
Bodo Tasche <bodo@wannawork.de>
2011-11-27 15:45:02 +0100

    fix merge conflicts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
+  
+  scope :recent, lambda { |limit = 3| order('created_at desc').limit(limit) }
 end
