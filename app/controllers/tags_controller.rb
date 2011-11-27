86dd8fc1283c36610f9338d83bdcbbd1426065b4
Bodo Tasche <bodo@wannawork.de>
2011-11-27 18:03:31 +0100

    SingleEvents sortieren

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,7 +10,7 @@ class TagsController < ApplicationController
   
   def show
     @events = Event.tagged_with(params[:tagname]).order(:name).to_a
-    @single_events = SingleEvent.in_future.tagged_with(params[:tagname]).sort_by(&:occurrence).delete_if {|o| o.occurrence > Date.today + 1.month}
+    @single_events = SingleEvent.in_future.tagged_with(params[:tagname]).delete_if {|o| o.occurrence > Date.today + 1.month}
     @events.each do |event|
       future_events = event.single_events.in_future.delete_if {|o| o.occurrence > Date.today + 1.month}
       if !future_events.empty?
@@ -18,6 +18,7 @@ class TagsController < ApplicationController
         @events.delete(event)
       end
     end
+    @single_events = @single_events.sort_by(&:occurrence)
   end
 
 end
