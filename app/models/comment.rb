665a72948399391158c460cdc16634aaef7b6744
moonglum <moonglum@moonbeamlabs.com>
2011-12-11 11:42:41 +0100

    Applying the Law of Demeter
    Handling loose coupling LIKE A BOSS.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
+  delegate :name, :to => :commentable, :prefix => true
+
   belongs_to :user
-  
+  delegate :nickname, :email, :to => :user, :prefix => true
+
   scope :recent, lambda { |limit = 3| order('created_at desc').limit(limit) }
 end
