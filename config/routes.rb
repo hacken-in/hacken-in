704d3bdda14098c986bbe3cc6c1ff0712fa9f6ad
moonglum <moonglum@moonbeamlabs.com>
2011-12-11 12:33:23 +0100

    Added some 'only's to the routes
    If your application defines many reoutes, the above restricting on
    routes will delete many useless routes, that means cutting down on
    memory use for you and speeding up the routing process.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Hcking::Application.routes.draw do
   devise_for :users
 
-  resources :users do
-    resources :hate_tags, controller: :user_hate_tags, :constraints => { :id => /.*/ }
+  resources :users, :only => [:show] do
+    resources :hate_tags, controller: :user_hate_tags, :constraints => { :id => /.*/ }, :only => [:create, :destroy]
   end
 
   match 'tags/:tagname'  => 'tags#show', :constraints => { :tagname => /.*/ }
-  resources :tags, :constraints => { :id => /.*/ }
+  resources :tags, :constraints => { :id => /.*/ }, :only => [:show, :index]
 
   resources :comments, :only => [:index]
 
   resources :events do
-    resources :comments
+    resources :comments, :only => [:show, :create, :edit, :update, :destroy, :index]
     namespace "schedule" do
-      resources :rdates, :exdates, :rules
+      resources :rdates, :only => [:create, :destroy]
+      resources :exdates, :only => [:create, :destroy]
+      resources :rules, :only => [:create, :destroy]
 
       match 'update' => 'update#update', :via => :put
     end
@@ -22,7 +24,7 @@ Hcking::Application.routes.draw do
         put :participate
         put :unparticipate
       end
-      resources :comments
+      resources :comments, :only => [:show, :create, :edit, :update, :destroy, :index]
     end
   end
 
@@ -33,5 +35,4 @@ Hcking::Application.routes.draw do
   root :to => 'welcome#index'
 
   match ':page_name' => 'pages#show'
-
 end
