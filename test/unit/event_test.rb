b30528b1964e3e27d5c391f7ed49887cacff5ba5
moonglum <moonglum@moonbeamlabs.com>
2011-12-18 00:01:32 +0100

    Moved the Dates in the test into the future
    The failure was due to Dates from the past. I moved
    them into the future, now everything passes!
    event.schedule.all_occurrences only contains events
    from the future!

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -6,22 +6,22 @@ class EventTest < ActiveSupport::TestCase
   test "can be saved" do
     event = Event.new(name: "Hallo")
     assert_equal 0, event.schedule.all_occurrences.size
-    event.schedule.add_recurrence_date(Time.new(2011,6,13,14,20,0,0))
+    event.schedule.add_recurrence_date(Time.new(2012,6,13,14,20,0,0))
     assert_equal 1, event.schedule.all_occurrences.size
     assert event.save
 
     event = Event.find_by_id(event.id)
     assert_equal 1, event.schedule.all_occurrences.size
-    assert_equal Time.new(2011,6,13,14,20,0,0), event.schedule.all_occurrences.first
+    assert_equal Time.new(2012,6,13,14,20,0,0), event.schedule.all_occurrences.first
 
     event = Event.new(name: "Hallo")
-    event.schedule_yaml = "--- \n:start_date: 2011-06-13 14:20:22 +02:00\n:rrules: []\n\n:exrules: []\n\n:rdates: \n- 2011-06-13 14:20:22 +02:00\n:exdates: []\n\n:duration: \n:end_time: \n"
+    event.schedule_yaml = "--- \n:start_date: 2012-06-13 14:20:22 +02:00\n:rrules: []\n\n:exrules: []\n\n:rdates: \n- 2012-06-13 14:20:22 +02:00\n:exdates: []\n\n:duration: \n:end_time: \n"
     assert_equal 1, event.schedule.all_occurrences.size
-    assert_equal Time.new(2011,6,13,14,20,22,"+02:00"), event.schedule.all_occurrences.first
+    assert_equal Time.new(2012,6,13,14,20,22,"+02:00"), event.schedule.all_occurrences.first
 
     event = Event.new(name: "Hallo")
     schedule = IceCube::Schedule.new(Time.new(2011,6,14,19,30))
-    schedule.add_recurrence_date(Time.new(2011,6,13,14,20,0,0))
+    schedule.add_recurrence_date(Time.new(2012,6,13,14,20,0,0))
     event.schedule = schedule
     assert_equal 1, event.schedule.all_occurrences.size
   end
