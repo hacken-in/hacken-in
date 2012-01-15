12cb7b22589282cadc12faa89ea5abb9bed365b7
Bodo Tasche <bodo@wannawork.de>
2012-01-15 09:23:35 +0100

    Show Event even if on hate list, when the user is a participant of that event

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,8 +5,9 @@ class WelcomeController < ApplicationController
 
     if user_signed_in? && !current_user.hate_list.empty?
       @single_events.delete_if do |single_event|
-        (single_event.event.tag_list & current_user.hate_list).length > 0
-      end
+        ((single_event.event.tag_list & current_user.hate_list).length > 0 &&
+        (!single_event.users.include? current_user))
+       end
     end
   end
 
