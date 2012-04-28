38e1b84a100cb742e4cef4a8fe13d1889a86f744
moonglum <moonglum@moonbeamlabs.com>
2012-04-28 16:19:48 +0200

    Use Yahoo as Geocoding API in Tests. They know no limits.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,2 +1,6 @@
-Geocoder::Configuration.lookup = :yahoo
-Geocoder::Configuration.api_key = "FZGMCffV34GyRHDpvcpT8NrASJtqaZ5_mdzn0gL5tAFGQg8Rv7Mgi5fkWHWyRgDU7A"
+if Rails.env.test?
+  Geocoder::Configuration.lookup = :yahoo
+  Geocoder::Configuration.api_key = "FZGMCffV34GyRHDpvcpT8NrASJtqaZ5_mdzn0gL5tAFGQg8Rv7Mgi5fkWHWyRgDU7A"
+else
+  Geocoder::Configuration.lookup = :google
+end
