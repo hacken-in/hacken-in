72e03b2b18a1715ebdad6d502126e6242a8d7a38
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-04-28 20:10:56 +0200

    Add fancy pants preview to Event#new
    Also generalized JS functions with this functionality.
    You can now register a preview hook as easy as:
    
    * provide a div.markdown-preview container somewhere on the page.
    * call registerPreviewHook('textarea#some_id')
    
    Aaaaand it's gone - ...done!

--- app/assets/javascripts/hcking.js
+++ app/assets/javascripts/hcking.js
@@ -1,3 +1,32 @@
 $(function() {
   $('.gravatar_tooltip').tipsy({gravity: 'nw'});
-})
+});
+
+function registerPreviewHook(identifier) {
+  $('a.show-preview').click(function(e) {
+    e.preventDefault();
+    showPreview(identifier);
+  });
+}
+      
+function showPreview(identifier) {
+  $(identifier).change(function() { 
+    updatePreview(identifier) 
+  }).keyup(function() { 
+    updatePreview(identifier)
+  });
+        
+  if ($('.markdown-preview').is(':hidden'))
+    $('.markdown-preview').slideDown();
+  else
+    $('.markdown-preview').slideUp();
+        
+  $(identifier).change();
+}
+      
+function updatePreview(identifier) {
+  var comment_text = $(identifier).val();
+  var generated_html = new Showdown.converter().makeHtml(comment_text);
+  $('.markdown-preview').html(generated_html);
+}
+
