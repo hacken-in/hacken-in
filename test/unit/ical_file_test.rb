4c115a8d1b47de1487e2bc65ea0d351860295c52
moonglum <moonglum@moonbeamlabs.com>
2012-04-28 20:34:33 +0200

    The whole iCalFile Concept was a mistake. Redid it.

--- test/unit/ical_file_test.rb
+++ test/unit/ical_file_test.rb
@@ -5,23 +5,6 @@ require 'test_helper'
 class IcalFileTest < ActiveSupport::TestCase
   setup :load_ical_file
 
-  test "parses the ical data from dingfabrik" do
-    assert_equal 69, @dingfabric_ical.number_of_events
-  end
-
-  test "iterates over the events" do
-    @dingfabric_ical.each_event do |event|
-      assert event.summary.length > 5
-      break
-    end
-  end
-
-  test "iterates over events that match a certain expression" do
-    @dingfabric_ical.each_event_with_pattern /Bastelnachmittag/ do |event|
-      assert_equal "Bastelnachmittag", event.summary
-    end
-  end
-
   protected
 
   def load_ical_file
