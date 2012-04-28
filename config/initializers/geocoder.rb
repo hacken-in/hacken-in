fe084bb47e573c37d60841f98b7901ecb53fddfb
moonglum <moonglum@moonbeamlabs.com>
2012-04-28 16:25:50 +0200

    That was nonsense. Now better that is.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,4 @@
 if Rails.env.test?
   Geocoder::Configuration.lookup = :yahoo
   Geocoder::Configuration.api_key = "FZGMCffV34GyRHDpvcpT8NrASJtqaZ5_mdzn0gL5tAFGQg8Rv7Mgi5fkWHWyRgDU7A"
-else
-  Geocoder::Configuration.lookup = :google
 end
