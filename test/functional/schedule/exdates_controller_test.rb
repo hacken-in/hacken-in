03b94b2afeb52c2d093bfe828412a134e7290c8c
Bodo Tasche <bodo@wannawork.de>
2012-04-29 13:27:00 +0200

    added missing test

--- test/functional/schedule/exdates_controller_test.rb
+++ test/functional/schedule/exdates_controller_test.rb
@@ -16,4 +16,18 @@ class Schedule::ExdatesControllerTest < ActionController::TestCase
     assert_redirected_to root_path
   end
 
+  test "should create and delete new exdate if bodo" do
+    event = FactoryGirl.create(:simple)
+
+    sign_in FactoryGirl.create(:bodo)
+    event = Event.find(event.id)
+    event.schedule.add_exception_time Time.new(1980,5,1,12,30)
+
+    delete :destroy, :event_id => event.id, :id => 0
+    assert_not_nil flash.notice
+    assert_redirected_to event_path(event)
+    event = Event.find(event.id)
+    assert_equal 0, event.schedule.extimes.count
+  end
+
 end
