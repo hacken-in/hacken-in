e613db40b00ad8340dcfb715e5b6e25b9d5b57e4
Bodo Tasche <bodo@wannawork.de>
2012-05-05 18:11:15 +0200

    add ical subscribe to tags page, finally fixes #55

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,6 +15,7 @@ class WelcomeController < ApplicationController
     begin
       @event = Event.find params[:event_id] if params[:event_id]
       @single_event = SingleEvent.find params[:single_event_id] if params[:single_event_id]
+      @tag = params[:tag_id]
     rescue ActiveRecord::RecordNotFound
       flash[:error] = "Event nicht gefunden"
     end
