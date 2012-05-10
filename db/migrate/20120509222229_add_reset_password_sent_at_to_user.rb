300ef0044e149057aeead00610b2c5e4a8e4439e
Bodo Tasche <bodo@wannawork.de>
2012-05-10 07:19:17 +0200

    shit, gitx fuckup. added too early to gitx and did not change the commit

--- db/migrate/20120509222229_add_reset_password_sent_at_to_user.rb
+++ db/migrate/20120509222229_add_reset_password_sent_at_to_user.rb
@@ -1,6 +1,6 @@
 class AddResetPasswordSentAtToUser < ActiveRecord::Migration
   def change
-    unless column_exists? :statuses, :hold_reason
+    unless column_exists? :users, :reset_password_sent_at
       add_column :users, :reset_password_sent_at, :datetime
     end
   end
