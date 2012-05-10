6ca5923ac801e0cfa3c491a87f21766d171dc204
Bodo Tasche <bodo@wannawork.de>
2012-05-10 07:15:49 +0200

    fixes travis
    explanation: I needed this migration for all systems that had
    already done the migration earlier to devise 2. With devise 2
    the rememeberal migration already does this. If you recreate
    the migrations from scratch, you will have this column already.

--- db/migrate/20120509222229_add_reset_password_sent_at_to_user.rb
+++ db/migrate/20120509222229_add_reset_password_sent_at_to_user.rb
@@ -1,5 +1,7 @@
 class AddResetPasswordSentAtToUser < ActiveRecord::Migration
   def change
-    add_column :users, :reset_password_sent_at, :datetime
+    unless column_exists? :statuses, :hold_reason
+      add_column :users, :reset_password_sent_at, :datetime
+    end
   end
 end
