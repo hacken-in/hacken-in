eadf85ffb5db488c2b648400996675f368d02b3d
moonglum <moonglum@moonbeamlabs.com>
2012-06-17 16:43:11 +0200

    Chaned the Devise Migration to Devise 2.0 style
    Following the guide [here](https://github.com/plataformatec/devise/wiki/How-To:-Upgrade-to-Devise-2.0-migration-schema-style)

--- db/migrate/20110327194043_devise_create_users.rb
+++ db/migrate/20110327194043_devise_create_users.rb
@@ -1,11 +1,27 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      # t.database_authenticatable null: false
-      t.recoverable
-      t.rememberable
-      t.trackable
-      t.encryptable
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Encryptable
+      t.string :password_salt
+
       t.timestamps
     end
 
