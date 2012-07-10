de33ad1dd62227a93f058d7a6fcd94d6822031aa
moonglum <moonglum@moonbeamlabs.com>
2012-07-10 22:26:44 +0200

    I don't like duplicate code

--- app/controllers/single_events_controller.rb
+++ app/controllers/single_events_controller.rb
@@ -62,28 +62,28 @@ class SingleEventsController < ApplicationController
   end
 
   def participate
-    @single_event = SingleEvent.find(params[:id])
-
-    if user_signed_in?
-      @single_event.users << current_user
-      flash[:notice] = t "single_events.participate.confirmation"
-    else
-      flash[:error] = t "devise.failure.unauthenticated"
-    end
-
-    respond_with @single_event, location: event_single_event_path(@single_event.event, @single_event)
+    change_participation params[:id],
+      :push,
+      t("single_events.participate.confirmation")
   end
 
   def unparticipate
-    @single_event = SingleEvent.find(params[:id])
+    change_participation params[:id],
+      :delete,
+      t("single_events.unparticipate.confirmation")
+  end
+
+  def change_participation(id, how, confirmation)
+    @single_event = SingleEvent.find id
 
     if user_signed_in?
-      @single_event.users.delete current_user
-      flash[:notice] = t "single_events.unparticipate.confirmation"
+      @single_event.users.send how, current_user
+      flash[:notice] = confirmation
     else
       flash[:error] = t "devise.failure.unauthenticated"
     end
 
-    respond_with @single_event, location: event_single_event_path(@single_event.event, @single_event)
+    respond_with @single_event,
+      location: event_single_event_path(@single_event.event, @single_event)
   end
 end
