6d67e932d340c0c68de6eccde090d497f6ffb224
Timo Schilling <timo@schilling.io>
2012-07-12 00:22:39 +0200

    simplify PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,13 @@
 class PagesController < ApplicationController
+  rescue_from ActionView::MissingTemplate, with: :missing
 
   def show
     @page_name = params[:page_name].to_s.gsub(/\W/,'')
-
-    unless partial_exists? @page_name
-      render 'missing', status: 404
-    end
   end
 
   private
 
-  def partial_exists?(partial)
-    VALID_PARTIALS.include?(partial)
+  def missing
+    render 'missing', status: 404
   end
-
-  def self.find_partials
-    Dir.glob(Rails.root.join('app', 'views', 'pages', '_*.haml')).map do |file|
-      file = Pathname.new(file).basename.to_s
-      # Strip leading _ and then everything from the first . to the end of the name
-      file.sub(/^_/, '').sub(/\..+$/, '')
-    end
-  end
-
-  # Do this once on boot
-  VALID_PARTIALS = PagesController.find_partials
 end
