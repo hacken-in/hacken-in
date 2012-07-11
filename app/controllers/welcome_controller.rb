0c5dedfa0fc402494c1d8c858eec93197ba6cf24
Timo Schilling <timo@schilling.io>
2012-07-12 00:10:32 +0200

    simplify WelcomeController

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @single_events = SingleEvent.in_next 4.weeks
-
-    if user_signed_in? && !current_user.hate_list.empty?
-      @single_events.delete_if do |single_event|
-        ((single_event.event.tag_list & current_user.hate_list).length > 0 &&
-        (!single_event.users.include? current_user))
-      end
-    end
+    @single_events = SingleEvent.in_next(4.weeks).for_user(current_user)
   end
 end
