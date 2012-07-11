7a09726f022a71a56ff4acc0fc352e50a46281ae
moonglum <moonglum@moonbeamlabs.com>
2012-07-11 11:37:15 +0200

    Simplified UserTagsController

--- app/controllers/user_tags_controller.rb
+++ app/controllers/user_tags_controller.rb
@@ -1,40 +1,37 @@
 # encoding: utf-8
 
 class UserTagsController < ApplicationController
-
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find params[:user_id]
 
-    # Only change the currently signed in user
-    if current_user == @user
-       @user.hate_list << params[:user_hate_tags][:hate_list] if params[:user_hate_tags] && params[:user_hate_tags][:hate_list]
-       @user.like_list << params[:user_like_tags][:like_list] if params[:user_like_tags] && params[:user_like_tags][:like_list]
-
-       if @user.save
-         flash[:notice] = "Tag hinzugefügt."
-       else
-         flash[:error] = "Es ist ein Fehler aufgetreten."
-       end
+    if modify_tag_list @user, params[:kind], push: params[:user_tags][:list]
+      flash[:notice] = t "user_tags.create.confirmation"
+    else
+      flash[:error] = t "user_tags.create.error"
     end
 
     redirect_to :root
   end
 
   def destroy
-    @user = User.find(params[:user_id])
+    @user = User.find params[:user_id]
 
-    # Only change the currently signed in user
-    if current_user == @user
-       @user.send(:"#{params[:kind]}_list").remove params[:id]
-
-       if @user.save
-         flash[:notice] = "Tag entfernt."
-       else
-         flash[:error] = "Es ist ein Fehler aufgetreten."
-       end
+    if modify_tag_list @user, params[:kind], remove: params[:id]
+      flash[:notice] = t "user_tags.destroy.confirmation"
+    else
+      flash[:error] = t "user_tags.destroy.error"
     end
 
     redirect_to :root
   end
 
+  private
+
+  def modify_tag_list(user, kind, action)
+    # Only change the currently signed in user
+    if current_user == user
+      user.modify_tag_list kind, action
+      user.save
+    end
+  end
 end
