ebb31b23b53c382f8b28d6825543c597530252ab
Klaus Zanders <klaus.zanders@gmail.com>
2012-07-14 13:26:40 +0200

    User is now omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   # Tags hated by the user
