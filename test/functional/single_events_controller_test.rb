bb9c87f67c1d009c4a529ad945a212c288de09cc
Timo Schilling <timo@schilling.io>
2012-07-17 01:55:11 +0200

    add some single_events_controller_test's

--- test/functional/single_events_controller_test.rb
+++ test/functional/single_events_controller_test.rb
@@ -11,6 +11,25 @@ class SingleEventsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should be successful as user" do
+    single_event = FactoryGirl.create(:single_event)
+    user = FactoryGirl.create(:bodo)
+    sign_in user
+
+    get :show, id: single_event.id, event_id: single_event.event.id
+    assert_response :success
+  end
+
+  test "should be successful as participated user" do
+    single_event = FactoryGirl.create(:single_event)
+    user = FactoryGirl.create(:bodo)
+    single_event.users << user
+    sign_in user
+
+    get :show, id: single_event.id, event_id: single_event.event.id
+    assert_response :success
+  end
+
   test "should render the comments partial in order to show and post comments" do
     single_event = FactoryGirl.create(:single_event)
     get :show, id: single_event.id, event_id: single_event.event.id
