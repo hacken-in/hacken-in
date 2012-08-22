a5f07261bb0256db579b1a42922d4e7ca1839b09
moonglum <moonglum@moonbeamlabs.com>
2012-08-22 21:27:14 +0200

    Comment on Comments
    Meta, Meta

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,2 +1,5 @@
+# This is called NerdhubComment for a technical reason:
+# If you call it Comment, there is a bug that basically
+# blows up everything ;)
 ActiveAdmin.register Comment, as: "NerdhubComment" do
 end
