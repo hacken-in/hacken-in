f18eb2c2a6085cc64219e135ef2164dea2fdef4d
Bodo Tasche <bodo@wannawork.de>
2012-08-26 16:46:53 +0200

    Show Picture in ActiveAdmin overview for #124

--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -1,6 +1,9 @@
 ActiveAdmin.register Picture do
   menu :label => "Bildergallerie"
   index do
+    column :image do |p|
+      image_tag p.image.url(:thumb)
+    end
     column :title
     column :description
     default_actions
