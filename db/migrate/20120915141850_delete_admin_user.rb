a3f2f56d4315d4d84ab717b69d046fef64e726a7
Bodo Tasche <bodo@wannawork.de>
2012-09-15 17:19:15 +0200

    AdminUser Migration deleted, not longer needed

--- db/migrate/20120915141850_delete_admin_user.rb
+++ db/migrate/20120915141850_delete_admin_user.rb
@@ -1,6 +1,6 @@
 class DeleteAdminUser < ActiveRecord::Migration
   def up
-    drop_table :admin_users
+    drop_table :admin_users if ActiveRecord::Base.connection.tables.include?("admin_users")
   end
 
   def down
