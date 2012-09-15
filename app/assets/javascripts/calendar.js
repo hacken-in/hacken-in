cc00a7352ccbac697a555472ca9758e7aa80dcce
Klaus Zanders <klaus.zanders@gmail.com>
2012-09-15 18:05:41 +0200

    Highlight des Kalender Events beim Mouseover

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  $('.calendar-line').on('mouseenter', function() {
+    $(this).animate({'background-color': $(this).data('hlcolor')}).addClass('calendar-line-highlighted');
+  });
+
+  $('.calendar-line').on('mouseleave', function() {
+    $(this).css({'background-color': '#000'}).removeClass('calendar-line-highlighted');
+  });
+});
