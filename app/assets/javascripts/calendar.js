870f8fd0d1e7e21580851a68b9967fb452d60fd0
Klaus Zanders <klaus.zanders@gmail.com>
2012-09-15 18:06:58 +0200

    Ohne Animation is cooler

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,10 +2,10 @@
 // All this logic will automatically be available in application.js.
 $(function() {
   $('.calendar-line').on('mouseenter', function() {
-    $(this).animate({'background-color': $(this).data('hlcolor')}).addClass('calendar-line-highlighted');
+    $(this).css('background-color', $(this).data('hlcolor')).addClass('calendar-line-highlighted');
   });
 
   $('.calendar-line').on('mouseleave', function() {
-    $(this).css({'background-color': '#000'}).removeClass('calendar-line-highlighted');
+    $(this).css('background-color', '#000').removeClass('calendar-line-highlighted');
   });
 });
