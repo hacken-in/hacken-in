c31c795f83de70abad2e311c6ed7727df46de399
Bodo Tasche <bodo@wannawork.de>
2012-09-15 16:56:26 +0200

    Twitter and Hashtg linked

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -13,8 +13,13 @@ ActiveAdmin.register Event do
     column :url do |event|
       a "Link", href: event.url
     end
-    column :twitter
-    column :twitter_hashtag
+    column :twitter do |event|
+      a "@#{event.twitter}", href: "http://twitter.com/#{event.twitter}" unless event.twitter.blank?
+    end
+    column :twitter_hashtag do |event|
+      a "##{event.twitter_hashtag}", href: "http://twitter.com/search/%23#{event.twitter_hashtag}" unless event.twitter_hashtag.blank?
+    end
+
     default_actions
   end
 end
