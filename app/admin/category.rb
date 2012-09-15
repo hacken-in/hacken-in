45f68a52fad663763bf3f04a320130231a7b596b
Bodo Tasche <bodo@wannawork.de>
2012-09-15 15:46:02 +0200

    added color

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,2 +1,11 @@
 ActiveAdmin.register Category do
+  index do
+    column :title do |p|
+      span p.title, style: "color: #{p.color}"
+    end
+    column :color do |p|
+      span p.color, style: "color: #{p.color}"
+    end
+    default_actions
+  end
 end
