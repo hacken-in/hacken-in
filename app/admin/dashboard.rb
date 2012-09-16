f9a293bbe5ef1fe3272ecb881480ca781859bbda
Bodo Tasche <bodo@wannawork.de>
2012-09-16 02:59:51 +0200

    dashboard must always be the first entry

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 0, label: proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     # Here is an example of a simple dashboard with columns and panels.
