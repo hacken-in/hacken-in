d75d3c11690cc415e9fc42c6006d66948b4bbece
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-09-23 18:28:21 +0200

    Display empty days in mini calendar
    These were hidden until now.
    
    This change introduced some smelly code in `SingleEvent`. It should be
    extracted to a domain object and tested in isolation. The current test
    broke and did not apply to the new feature, so I removed it.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index
     single_events = SingleEvent.recent_to_soon(4.weeks)
 
-    @single_events_by_day = SingleEvent.catalog_by_day(single_events)
+    @single_events_by_day = SingleEvent.catalog_by_day(single_events, Date.today - 4.weeks, Date.today + 4.weeks)
   end
 
 end
