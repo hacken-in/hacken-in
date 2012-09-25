352cd85fa85767ed63c14908327d1322950c132d
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-09-25 20:45:13 +0200

    Refactor Calendar implementation
    Thanks to @klaustopher for suggesting very helpful Rails mixins

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -2,25 +2,17 @@
 class Calendar
 
   def self.events_by_day(events)
-    events_by_day = Hash.new { |hash, key| hash[key] = [] }
-
-    events.each do |event|
-      events_by_day[event.occurrence.to_date] << event
-    end
-
-    events_by_day
+    events.group_by { |event| event.occurrence.to_date }
   end
 
   def self.fill_gaps(days, first_day, last_day, min_entries=8)
 
     (first_day..last_day).each do |day|
-      days[day] = [] unless days.key? day
-      events_for_day = days[day]
+      days[day] = Array.new(min_entries) unless days.key?(day)
 
-      if events_for_day.size < min_entries
-        (min_entries - events_for_day.size).times do
-          days[day] << nil
-        end
+      if days[day].size < min_entries
+        gaps_filled = days[day].in_groups_of(min_entries).first
+        days[day] = gaps_filled
       end
     end
 
