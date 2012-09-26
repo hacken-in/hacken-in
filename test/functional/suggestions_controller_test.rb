1757241559da5528efe8465a12c7f8926c4706a4
moonglum <moonglum@moonbeamlabs.com>
2012-09-26 10:49:24 +0200

    Fixed functional test

--- test/functional/suggestions_controller_test.rb
+++ test/functional/suggestions_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class SuggestionsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
+  test "should get show" do
+    suggestion = FactoryGirl.create(:suggestion)
+
+    get :show, id: suggestion.id
     assert_response :success
   end
-
 end
