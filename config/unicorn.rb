68117eab093839ce0fa26b57c17223b65216ad0f
Bodo Tasche <bodo@wannawork.de>
2012-10-27 13:24:32 +0200

    removing preoload_app , it is making problems with the vlad unicorn thing

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,38 +9,3 @@ worker_processes 2
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
-
-# Preload our app for more speed
-preload_app true
-
-before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
-  # Before forking, kill the master process that belongs to the .oldbin PID.
-  # This enables 0 downtime deploys.
-  old_pid = "/var/www/virtual/droidboy/nerdhub/shared/pids/unicorn.pid.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
-
-after_fork do |server, worker|
-  # the following is *required* for Rails + "preload_app true",
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
-
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
-end
