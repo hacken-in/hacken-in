4abf08bade83fac22854002eaf0b4a3a1e6ad532
moonglum <moonglum@moonbeamlabs.com>
2012-10-27 19:50:10 +0200

    Schemata are quite important a wise man once said
    Kids, let me tell you this:
    When you use a relational database, always have a schema.
    It's quite useful.
    And things break without it.
    Why do they need a schema, you may ask.
    
    Don't ask me. I work on a NoSQL database.
    :trollface:

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20121027163815) do
+
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
     t.string   "resource_type", :null => false
