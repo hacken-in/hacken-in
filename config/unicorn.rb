738d036b65a466497764c55a3a142ebc3b8b1281
Bodo Tasche <bodo@wannawork.de>
2012-10-27 12:56:25 +0200

    wrong pid file :)

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -22,7 +22,7 @@ before_fork do |server, worker|
 
   # Before forking, kill the master process that belongs to the .oldbin PID.
   # This enables 0 downtime deploys.
-  old_pid = "/var/run/adcloud-core/adcloud-core.pid.oldbin"
+  old_pid = "/var/www/virtual/droidboy/nerdhub/shared/pids/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
