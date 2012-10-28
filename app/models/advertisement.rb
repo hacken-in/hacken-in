2458af156d2c94b03159cce6a8848e8ad8f85d6c
moonglum <moonglum@moonbeamlabs.com>
2012-10-28 15:00:47 +0100

    Advertisement model: Get homepage ad

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,6 +1,10 @@
+require "calendar_week"
+
 class Advertisement < ActiveRecord::Base
   attr_accessible :calendar_week, :description, :duration, :file, :link, :context, :picture_id
   belongs_to :picture
 
-
+  def Advertisement.homepage
+    Advertisement.where(context: 'homepage').where(calendar_week: DateTime.now.iso_cweek).first
+  end
 end
--- test/unit/advertisement_test.rb
+++ test/unit/advertisement_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class AdvertisementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "find only advertisement with the right context for the homepage" do
+    FactoryGirl.create :advertisement, context: "blog_post", description: "not me"
+    FactoryGirl.create :advertisement, context: "homepage", description: "but me"
+    ad = Advertisement.homepage
+
+    assert_equal "but me", ad.description
+  end
+
+  test "find the advertisement starting this week for the homepage" do
+    FactoryGirl.create :advertisement,
+      context: "homepage",
+      description: "not me",
+      calendar_week: (DateTime.now + 2.weeks).iso_cweek
+    FactoryGirl.create :advertisement,
+      context: "homepage",
+      description: "but me",
+      calendar_week: DateTime.now.iso_cweek
+    ad = Advertisement.homepage
+
+    assert_equal "but me", ad.description
+  end
 end
