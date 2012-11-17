32b47d6e0088e4ccb035904d2dc7236eaab9854e
Tobias Eilert <tobias.eilert@tux-mail.de>
2012-11-17 11:59:41 +0100

    Remove event history

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,10 +13,4 @@ class EventsController < ApplicationController
     opengraph_data @event.to_opengraph
     respond_with @event
   end
-
-  def history
-    authorize! :history, SingleEvent
-    @events = SingleEvent.where("occurrence < ?", Time.now.at_beginning_of_day)
-    respond_with @events
-  end
 end
