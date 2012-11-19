b365253b30902ad10954667cf13d6766fdcc181c
Klaus Zanders <klaus.zanders@gmail.com>
2012-11-19 20:02:13 +0100

    Erster Anlauf für die deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,24 @@
+# -*- encoding : utf-8 -*-
+require "bundler/capistrano"
+
+# Settings
 set :application, "droidboy"
 set :deploy_to, "/var/www/virtual/droidboy/nerdhub"
 set :user, "droidboy"
-set :domain, "#{user}@corvus.uberspace.de"
+
+# Git Repo
 set :repository,  "git://github.com/nerdhub/hcking.git"
 set :scm, :git
+set :branch, "master"
 
-role :web, "droidboy"
-role :app, "droidboy"
-role :db,  "droidboy", :primary => true
+# SSH Options
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+# Servers
+role :web, "corvus.uberspace.de"
+role :app, "corvus.uberspace.de"
+role :db,  "corvus.uberspace.de", :primary => true
 
 after "deploy:restart", "deploy:cleanup"
 
@@ -18,4 +29,10 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    #ToDo: Bodo, hier müssen alle Configs rein, die sonst noch gesymlinkt werden müssen
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
 end
