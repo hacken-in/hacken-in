9be7180d587e5ee03823502bd04cf07621d50537
Klaus Zanders <klaus.zanders@gmail.com>
2012-11-19 19:51:14 +0100

    Vlad raus, Capistrano rein, Step 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,21 @@
-require "bundler/vlad"
-
 set :application, "droidboy"
 set :deploy_to, "/var/www/virtual/droidboy/nerdhub"
 set :user, "droidboy"
 set :domain, "#{user}@corvus.uberspace.de"
-set :repository, 'git://github.com/nerdhub/hcking.git'
+set :repository,  "git://github.com/nerdhub/hcking.git"
+set :scm, :git
 
-set :config_files, ['database.yml', 'newrelic.yml', 'initializers/secret_token.rb', 'omniauth.yml']
+role :web, "droidboy"
+role :app, "droidboy"
+role :db,  "droidboy", :primary => true
 
-namespace :vlad do
+after "deploy:restart", "deploy:cleanup"
 
-  desc "Copy config files from shared/config to release dir"
-  remote_task :copy_config_files, roles: :app do
-    config_files.each do |filename|
-      run "cp #{shared_path}/config/#{filename} #{release_path}/config/#{filename}"
-    end
-    run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
-    run "rm #{release_path}/public/thisiscologne.json;ln -s #{shared_path}/thisiscologne.json #{release_path}/public/thisiscologne.json"
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-
-  desc "Make a call to the passenger to create a running instance"
-  remote_task :call_passenger, roles: :app do
-    run "wget -O ~/test.html http://hcking.de;rm ~/test.html"
-  end
-
-  desc "Regenerate assets"
-  remote_task :regenerate_assets, roles: :app do
-    run "cd #{release_path};RAILS_ENV=production bundle exec rake assets:precompile"
-  end
-
 end
-
