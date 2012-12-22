7cf9a9a1a914547958fa700d0b7af421bc4196d2
Bodo Tasche <bodo@wannawork.de>
2012-12-22 01:34:13 +0100

    asset:precompile hopefully works better with this

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,11 +45,22 @@ namespace :deploy do
 
     # Check for changes in assets dir and only precompile if there are any
     task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      # Check if assets have changed. If not, don't run the precompile task - it takes a long time.
+      force_compile = false
+      changed_asset_count = 0
+      begin
+        from = source.next_revision(current_revision)
+        asset_locations = 'app/assets/ lib/assets vendor/assets'
+        changed_asset_count = capture("cd #{latest_release} && #{source.local.log(from)} #{asset_locations} | wc -l").to_i
+      rescue Exception => e
+        logger.info "Error: #{e}, forcing precompile"
+        force_compile = false
+      end
+      if changed_asset_count > 0 || force_compile
+        logger.info "#{changed_asset_count} assets have changed. Pre-compiling"
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
       else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
+        logger.info "#{changed_asset_count} assets have changed. Skipping asset pre-compilation"
       end
     end
 
