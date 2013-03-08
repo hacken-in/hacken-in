5d04876c288c24253c8d35ac616b78c41a15bc6b
Timo Schilling <timo@schilling.io>
2013-03-08 01:28:48 +0100

    refactor uploaders #2 inherit CarouselUploader from PictureUploader

--- app/uploaders/carousel_uploader.rb
+++ app/uploaders/carousel_uploader.rb
@@ -1,9 +1,2 @@
-class CarouselUploader < AbstractUploader
-  version :thumb do
-    process :resize_to_fill => [50, 50]
-  end
-
-  version :small do
-    process :resize_to_fill => [307, 177]
-  end
+class CarouselUploader < PictureUploader
 end
