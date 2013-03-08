6b0885c5924927b778d330bf0207aa27749dd9b7
Timo Schilling <timo@schilling.io>
2013-03-08 02:18:40 +0100

    remove uploads from picture factory

--- test/factories/picture.rb
+++ test/factories/picture.rb
@@ -2,8 +2,6 @@ include ActionDispatch::TestProcess
 
 FactoryGirl.define do
   factory :picture do
-    carousel_image { fixture_file_upload(Rails.root.to_s + '/app/assets/images/test-werbung.png', "image/png") }
-    box_image { fixture_file_upload(Rails.root.to_s + '/app/assets/images/test-werbung.png', "image/png") }
     title "Some title"
   end
 end
