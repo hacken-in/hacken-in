75479a68ee76c93f36f0251f4f4c811f5b932c20
Bodo Tasche <bodo@wannawork.de>
2013-03-30 17:25:45 +0100

    EventController moved to rspec #235

---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe EventsController do
+  include Devise::TestHelpers
+
+  it "should redirect to calendar if there are no single events" do
+    event = FactoryGirl.create(:simple)
+    get :show, id: event.id
+    expect(response).to redirect_to(:calendar)
+  end
+
+  it "should redirect to the best fitting single event" do
+    event = FactoryGirl.create(:simple)
+    single_event = FactoryGirl.create(:single_event, event: event)
+
+    Event.should_receive(:find).with(event.id.to_s).and_return(event)
+    event.should_receive(:closest_single_event).and_return(single_event)
+
+    get :show, id: event.id
+    expect(response).to redirect_to(event_single_event_path(event, single_event))
+  end
+
+  it "should redirect to calender page" do
+    get :index
+    expect(response).to redirect_to(:calendar)
+  end
+
+end
