2da2abf7a1e739bac962d92e883fa6ebb18edd60
Tobias Eilert <tobias.eilert@tux-mail.de>
2013-03-30 15:08:44 +0100

    Loginform jetzt nutzbar #141
    Registrierung fehlt noch

--- app/assets/javascripts/modules/loginform_popup.js
+++ app/assets/javascripts/modules/loginform_popup.js
@@ -2,8 +2,8 @@ $(function() {
   var login_link = $('.main-nav .login-nav-link');
 
   if (login_link.size() > 0) {
-    $(login_link).click(function() {
-      //alert("login");
+    login_link.click(function() {
+      $('.loginform.loginform-popup').toggleClass('visible');
       return false;
     });
   }
