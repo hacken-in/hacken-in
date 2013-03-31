63cb9f207df122cc6ecb7da0525910cd579ef993
moonglum <moonglum@moonbeamlabs.com>
2013-03-31 20:13:51 +0200

    Translated Participates Controller Test to Spec

--- spec/controllers/participates_controller_spec.rb
+++ spec/controllers/participates_controller_spec.rb
@@ -1,26 +1,26 @@
-require 'test_helper'
+require 'spec_helper'
 
-class ParticipatesControllerTest < ActionController::TestCase
+def assert_response_for single_event, format
+  assert_template ["single_events/_participants", "participates/update"] if format == :js
+  assert_redirected_to event_single_event_path(single_event.event, single_event) if format == :html
+end
+
+describe ParticipatesController do
   include Devise::TestHelpers
 
-  def assert_response_for single_event, format
-    assert_template ["single_events/_participants", "participates/update"] if format == :js
-    assert_redirected_to event_single_event_path(single_event.event, single_event) if format == :html
-  end
-
   [:html, :js].each do |format|
-    test "should create participate with format #{format}" do
+    it "should create participate with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
       user = FactoryGirl.create(:bodo)
       sign_in user
 
       post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push
 
-      assert_equal user, single_event.users.first
+      single_event.users.first.should == user
       assert_response_for single_event, format
     end
 
-    test "should delete participate with format #{format}" do
+    it "should delete participate with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
       user = FactoryGirl.create(:bodo)
       single_event.users << user
@@ -30,22 +30,22 @@ class ParticipatesControllerTest < ActionController::TestCase
 
       single_event.reload
 
-      assert_equal 0, single_event.users.length
+      single_event.users.length.should == 0
       assert_response_for single_event, format
     end
 
-    test "should not create participate if not logged in with format #{format}" do
+    it "should not create participate if not logged in with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
-      user = FactoryGirl.create(:bodo)
+      FactoryGirl.create(:bodo)
 
       post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push
 
-      assert_equal 0, single_event.users.length
-      assert_not_nil flash[:error]
+      single_event.users.length.should == 0
+      flash[:error].should_not be_nil
       assert_response_for single_event, format
     end
 
-    test "should not delete participate if not logged in with format #{format}" do
+    it "should not delete participate if not logged in with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
       user = FactoryGirl.create(:bodo)
       single_event.users << user
@@ -54,12 +54,12 @@ class ParticipatesControllerTest < ActionController::TestCase
 
       single_event.reload
 
-      assert_equal 1, single_event.users.length
-      assert_not_nil flash[:error]
+      single_event.users.length.should == 1
+      flash[:error].should_not be_nil
       assert_response_for single_event, format
     end
 
-    test "should not create participate two times for the same event with format #{format}" do
+    it "should not create participate two times for the same event with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
       user = FactoryGirl.create(:bodo)
       sign_in user
@@ -67,20 +67,20 @@ class ParticipatesControllerTest < ActionController::TestCase
       post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push
       post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push
 
-      assert_equal 1, single_event.users.length
+      single_event.users.length.should == 1
       assert_response_for single_event, format
     end
 
-    test "should not delete participate if not participant of event with format #{format}" do
+    it "should not delete participate if not participant of event with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
-      user = FactoryGirl.create(:bodo)
+      FactoryGirl.create(:bodo)
 
       post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :delete
 
       single_event.reload
 
-      assert_equal 0, single_event.users.length
-      assert_not_nil flash[:error]
+      single_event.users.length.should == 0
+      flash[:error].should_not be_nil
       assert_response_for single_event, format
     end
   end
