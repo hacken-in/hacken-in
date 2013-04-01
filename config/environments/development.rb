dffefc6162f8ec11f909076dde9dd09dc8650827
Klaus Zanders <klaus.zanders@gmail.com>
2013-04-01 11:11:28 +0200

    Use assets group in development and test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ Hcking::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.assets.debug = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
