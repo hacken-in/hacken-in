99e9b2072ddeeb5adb1b6ae1c1be0f09f8b0551a
Bodo Tasche <bodo@wannawork.de>
2013-05-09 23:27:07 +0200

    Removed blogpost, welcome page and carousel images

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 require 'carrierwave/orm/activerecord'
 
 class Picture < ActiveRecord::Base
-  attr_accessible :description, :box_image, :carousel_image, :title
+  attr_accessible :description, :box_image, :title
   mount_uploader :box_image, PictureUploader
   default_scope order(:title)
 end
