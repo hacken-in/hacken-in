1858a07c29da11eede1203f0a37af26d0b1b263e
Klaus Zanders <klaus.zanders@gmail.com>
2013-05-09 13:15:31 +0200

    Datenbank Seeds aufgeräumt

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,6 @@ ActiveRecord::Base.connection.tables.delete_if{|table| table == 'schema_migratio
   ActiveRecord::Base.connection.execute "TRUNCATE #{table}"  unless table == 'users'
 end
 
-
-
 ## ---- categories -----
 
 categories = [
@@ -31,26 +29,6 @@ categories.each do |category|
 end
 
 programming_category = Category.find_by_title("Programmieren")
-
-
-post = BlogPost.create(
-  headline: "Lorem ipsum blogpost headline",
-  headline_teaser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque libero elit, elementum et vestibulum a, dignissim non erat.",
-  teaser_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque libero elit, elementum et vestibulum a, dignissim non erat. Cras tincidunt, erat nec molestie faucibus, libero neque sodales tortor, at gravida libero elit vel odio. Duis ullamcorper lorem eget velit tincidunt ut pellentesque sem fermentum.",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque libero elit, elementum et vestibulum a, dignissim non erat. Cras tincidunt, erat nec molestie faucibus, libero neque sodales tortor, at gravida libero elit vel odio. Duis ullamcorper lorem eget velit tincidunt ut pellentesque sem fermentum. Praesent ut quam sit amet augue sagittis tristique. Suspendisse hendrerit, velit non imperdiet ultricies, est urna dictum eros, nec dictum magna eros sollicitudin metus. Ut luctus, nulla vitae ultrices consectetur, turpis nibh porttitor lacus, ullamcorper egestas ante purus ut justo. Ut suscipit vehicula nisl, in adipiscing leo semper vel. Etiam varius sagittis leo, vitae commodo odio pellentesque vel. In id nunc in dolor sodales semper. Etiam mattis eros nunc, quis lobortis dui. Vestibulum porttitor tincidunt lacus, a dictum arcu gravida et. Donec et augue dictum ligula lacinia tincidunt et sodales tortor. Etiam dapibus tempor dui, sed bibendum augue tempus eget. In hac habitasse platea dictumst.
-
-    Morbi scelerisque eros non erat posuere rutrum. Morbi eleifend pretium augue vitae faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent vitae quam nec tortor ullamcorper euismod sit amet sed orci. Vivamus quis ultricies massa. Nunc pulvinar magna id metus accumsan in posuere quam rhoncus. Aliquam ac magna nisl. Cras a lectus sed dui rutrum porta eget quis mi. Vestibulum dignissim pellentesque nulla, sagittis adipiscing elit consequat at. Praesent eget augue sit amet diam fringilla sollicitudin vitae eu ligula. Sed placerat luctus leo, nec volutpat purus porttitor nec. Phasellus nec ante eu nulla euismod adipiscing. Aliquam ut urna sed sem dignissim mattis. Praesent semper nisi sed nisi interdum eu eleifend odio ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-    Sed porttitor tellus a lorem vulputate pharetra. Aliquam et neque mi. Nulla auctor odio vitae lacus fringilla id auctor orci porttitor. Aliquam velit dolor, condimentum a feugiat eget, auctor eget lorem. Cras ac nisi arcu. In vestibulum pellentesque est, quis ultricies neque tempus at. Fusce augue dolor, molestie at laoreet sed, malesuada nec nisi. Proin pretium erat vel urna hendrerit congue ac at est. Duis ac urna arcu, non lacinia libero. Praesent semper, mi in laoreet molestie, sem eros lacinia tellus, pulvinar convallis neque elit nec nulla. Praesent sed ante justo. Etiam vel risus libero. Curabitur purus lectus, blandit in rhoncus non, malesuada eu lacus. Donec quam dui, facilisis id lacinia nec, molestie a ligula. Sed non nunc ante, sit amet placerat dolor. Phasellus et diam ante, ut viverra ipsum.
-
-    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam vel nisi eget nulla consequat rhoncus. Praesent hendrerit iaculis magna, nec tincidunt orci pulvinar pretium. Donec facilisis sem vitae erat bibendum vel convallis libero venenatis. In ac velit sit amet felis vulputate tempus. Duis porta dui eget eros suscipit elementum. Aliquam erat volutpat. Cras eu est vitae ligula convallis vulputate at vel mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-
-    Phasellus porttitor tortor ut risus gravida nec iaculis magna molestie. Mauris ut odio elit, a dignissim magna. Donec sed laoreet augue. Etiam turpis risus, facilisis sit amet fermentum id, lobortis eu metus. Vestibulum non augue sed massa laoreet ultrices et vehicula augue. In in tortor urna. Maecenas nec quam sed sem consectetur aliquet. In vehicula, purus sit amet auctor accumsan, libero ligula porta mauris, quis semper turpis ipsum at quam. Ut volutpat mattis nisi, ac blandit nunc interdum ac. Fusce scelerisque pretium nibh, quis elementum velit consequat id. Morbi sit amet orci ligula. Etiam cursus adipiscing consectetur. Ut volutpat placerat lorem, at gravida mauris egestas eget. Nullam a mauris id turpis molestie sollicitudin eget sit amet lacus. Suspendisse potenti.",
-  user: User.first,
-  category: Category.first,
-  publishable: true,
-  publishable_from: Date.yesterday
-)
 
 # ---- venues -----
 
