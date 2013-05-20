9a7e590d0a89475bc310c38ceceea748a9a8f037
moonglum <moonglum@moonbeamlabs.com>
2013-05-20 09:59:53 +0200

    Fixed Security Bug: Executing arbitrary method :evil:
    Any method could be called on a user object.
    Not a good idea.

--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -5,8 +5,8 @@ class ParticipatesController < ApplicationController
     @single_event = SingleEvent.find params[:single_event_id]
 
     if user_signed_in?
-      @single_event.users.send params[:state].to_sym, current_user
-      flash[:notice] = t("single_events.#{params[:state]}.confirmation")
+      push_or_delete(params[:state].to_s)
+      flash[:notice] = t "single_events.#{params[:state]}.confirmation"
     else
       flash[:error] = t "devise.failure.unauthenticated"
     end
@@ -14,6 +14,15 @@ class ParticipatesController < ApplicationController
     respond_with @single_event do |format|
       format.html { redirect_to event_single_event_path(@single_event.event, @single_event) }
       format.js
+    end
+  end
+
+  private
+
+  def push_or_delete(action)
+    case action
+    when "push" then @single_event.users.push(current_user)
+    when "delete" then @single_event.users.delete(current_user)
     end
   end
 end
