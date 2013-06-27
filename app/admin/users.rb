7113b836e238b6cfd03fc57185492d99c7c5c6c5
Klaus Zanders <klaus.zanders@gmail.com>
2013-06-27 21:51:25 +0200

    [Admin] Die Benutzer etwas sparsamer anzeigen, Event Kurationen zuweisen, Regionen zuweisen

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -18,6 +18,25 @@ ActiveAdmin.register User do
     render partial: 'form'
   end
 
+  show do
+    attributes_table do
+      row :id
+      row :nickname
+      row :name
+      row :email
+      row :current_sign_in_at
+      row :admin
+      row :current_region
+      row :assigned_regions do |u|
+        raw u.assigned_regions.map { |region| link_to region.name, admin_region_path(region) }.join(', ')
+      end
+      row :curated_events do |u|
+        raw u.curated_events.map { |event| link_to event.title, admin_event_path(title) }.join(', ')
+      end
+    end
+  end
+
+
   controller do
     with_role :admin
     defaults finder: :find_by_nickname
