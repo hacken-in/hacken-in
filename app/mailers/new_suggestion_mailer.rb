b87354b93072695d0f40da0585c43023671caae7
Bodo Tasche <bodo@wannawork.de>
2013-07-19 21:08:40 +0200

    added klaus to list of recipients

--- app/mailers/new_suggestion_mailer.rb
+++ app/mailers/new_suggestion_mailer.rb
@@ -2,7 +2,7 @@
 class NewSuggestionMailer < ActionMailer::Base
   default from: "bodo@wannawork.de"
 
-  RECIPIENTS = ["bodo@wannawork.de", "lucas.dohmen@koeln.de"]
+  RECIPIENTS = ["bodo@wannawork.de", "lucas.dohmen@koeln.de", "klaus.zanders@gmail.com"]
 
   def new_suggestion(record)
     @record = record
