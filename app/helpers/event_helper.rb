23587d9dd6afa15e9b52672976e6dcb7857e6145
Klaus Zanders <klaus.zanders@gmail.com>
2013-07-20 22:58:59 +0200

    Im SingleEvent auch ein Formular für die externen Teilnehmer anzeigen

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -2,4 +2,12 @@ module EventHelper
   def tag_list_for_taggable(event)
     event.tag_list.collect { |tag| { name: tag } }.to_json.html_safe
   end
+
+  def external_participation_data
+    begin
+    JSON.parse(cookies.signed[:hacken_daten] || "{}", symbolize_names: true)
+    rescue
+      {}
+    end
+  end
 end
