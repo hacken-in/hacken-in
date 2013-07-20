c9385613c0e354ff211e4e14e221735609f0dff9
Klaus Zanders <klaus.zanders@gmail.com>
2013-07-20 22:58:13 +0200

    Participates Controller kann nun auch externe Leute teilnehmen lassen

--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -5,11 +5,15 @@ class ParticipatesController < ApplicationController
   def update
     @single_event = SingleEvent.find params[:single_event_id]
 
-    push_or_delete(params[:state].to_s)
-    flash[:notice] = t "single_events.#{params[:state]}.confirmation"
+    if push_or_delete(params[:state].to_s)
+      flash[:notice] = t "single_events.#{params[:state]}.confirmation"
+      redirect_params = {}
+    else
+      redirect_params = { email: params[:email], name: params[:name] }
+    end
 
     respond_with @single_event do |format|
-      format.html { redirect_to event_single_event_path(@single_event.event, @single_event) }
+      format.html { redirect_to event_single_event_path(@single_event.event, @single_event, redirect_params) }
       format.js
     end
   end
@@ -36,23 +40,37 @@ class ParticipatesController < ApplicationController
     if action == "push"
       # Eine UUID im Cookie speichern, damit der Nutzer nachträglich noch
       # sagen kann, dass er doch nicht teilnehmen möchte
-      external_user_session_token = (cookie.permanent[:hacken_uuid] ||= SecureRandom.uuid)
+      external_user_session_token = if cookies[:hacken_uuid].present?
+                                      cookies[:hacken_uuid]
+                                    else
+                                      cookies.permanent.signed[:hacken_uuid] = SecureRandom.uuid
+                                    end
+
       single_event_user = @single_event.external_users.new(email: params[:email], name: params[:name], session_token: external_user_session_token)
 
       if single_event_user.save
         # Wenn keep_data gesetzt ist, email und name in einem permanenten Cookie ablegen
         # sodass es beim nächsten mal wieder verwendet werden kann
         if params[:keep_data]
-          cookie.permanent[:hacken_daten] = {email: params[:email], name: params[:name]}
+          cookies.permanent.signed[:hacken_daten] = {email: params[:email], name: params[:name]}.to_json
+        else
+          cookies.delete(:hacken_daten)
         end
       else
-        # TODO: Fehler beim Speichern behandeln
+        flash[:error] = single_event_user.errors.full_messages.to_sentence
+        return false
       end
     elsif action == "delete"
-      single_event_user = @single_event.external_users.find_by_session_token(cookie[:hacken_uuid])
-      if single_event_user
+      single_event_user = @single_event.external_users.find_by_session_token(cookies[:hacken_uuid])
+      if cookies[:hacken_uuid].present? && single_event_user
         single_event_user.destroy
+      else
+        #TODO: Localize
+        flash[:error] = "Du nimmst an diesem Event nicht teil"
+        return false
       end
     end
+
+    true
   end
 end
--- spec/controllers/participates_controller_spec.rb
+++ spec/controllers/participates_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-def assert_response_for single_event, format
+def assert_response_for single_event, format, query_params={}
   assert_template ["single_events/_participants", "participates/update"] if format == :js
-  assert_redirected_to event_single_event_path(single_event.event, single_event) if format == :html
+  assert_redirected_to event_single_event_path(single_event.event, single_event, query_params) if format == :html
 end
 
 describe ParticipatesController do
@@ -34,28 +34,86 @@ describe ParticipatesController do
       assert_response_for single_event, format
     end
 
-    it "should not create participate if not logged in with format #{format}" do
+    it "should not create external participation if no email provided with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
-      FactoryGirl.create(:bodo)
 
-      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push
+      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push, name: "Hans Wurst"
 
-      single_event.users.length.should == 0
+      single_event.external_users.length.should == 0
       flash[:error].should_not be_nil
+      assert_response_for single_event, format, { name: "Hans Wurst" }
+    end
+
+    it "should not create external participation if invalid email provided with format #{format}" do
+      single_event = FactoryGirl.create(:single_event)
+
+      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push, name: "Hans Wurst", email: "hans#wurst.de"
+
+      single_event.external_users.length.should == 0
+      flash[:error].should_not be_nil
+      assert_response_for single_event, format, { name: "Hans Wurst", email: "hans#wurst.de" }
+    end
+
+    it "should create external participation if not logged in with format #{format}" do
+      single_event = FactoryGirl.create(:single_event)
+
+      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push, name: "Hans Wurst", email: "hans@wurst.de"
+
+      single_event.external_users.length.should == 1
+      flash[:error].should be_nil
       assert_response_for single_event, format
     end
 
-    it "should not delete participate if not logged in with format #{format}" do
+    it "should store a uuid in a permanent cookie when an external participation is saved with format #{format}" do
       single_event = FactoryGirl.create(:single_event)
-      user = FactoryGirl.create(:bodo)
-      single_event.users << user
+
+      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push, name: "Hans Wurst", email: "hans@wurst.de"
+
+      cookies[:hacken_uuid].should_not be_nil
+    end
+
+    it "should not generate a new uuid if one is already set with format #{format}" do
+      single_event = FactoryGirl.create(:single_event)
+      old_cookie_value = cookies[:hacken_uuid] = "ich_habe_schon_eine_id"
+
+      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push, name: "Hans Wurst", email: "hans@wurst.de"
+
+      cookies[:hacken_uuid].should == old_cookie_value
+    end
+
+
+    it "should store the participants data in a cookie, when the keep_data parameter is passed in with format #{format}" do
+      single_event = FactoryGirl.create(:single_event)
+
+      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :push, name: "Hans Wurst", email: "hans@wurst.de", keep_data: "1"
+
+      JSON.parse(cookies.signed[:hacken_daten] || "{}", symbolize_names: true).should == { email: "hans@wurst.de", name: "Hans Wurst" }
+    end
+
+    it "should not delete external participation if no uuid is present in cookie with format #{format}" do
+      single_event = FactoryGirl.create(:single_event)
+      single_event.external_users.create(name: "Hans Wurst", email:"hans@wurst.de", session_token: "abc123")
 
       post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :delete
 
       single_event.reload
 
-      single_event.users.length.should == 1
+      single_event.external_users.length.should == 1
       flash[:error].should_not be_nil
+      assert_response_for single_event, format
+    end
+
+    it "should delete external participation if uuid is present in cookie with format #{format}" do
+      single_event = FactoryGirl.create(:single_event)
+      single_event.external_users.create(name: "Hans Wurst", email:"hans@wurst.de", session_token: "abc123")
+      cookies[:hacken_uuid] = "abc123"
+
+      post :update, single_event_id: single_event.id, event_id: single_event.event.id, format: format, state: :delete
+
+      single_event.reload
+
+      single_event.external_users.length.should == 0
+      flash[:error].should be_nil
       assert_response_for single_event, format
     end
 
