8daf8153fb3c88e96ecfbf2d8cb6e75731b7097c
Bodo Tasche <bodo@wannawork.de>
2013-08-03 07:26:12 +0200

    added google map link
    …maybee we should add a nice little map directly to the admin interface to show the location :wink:

--- app/admin/venue.rb
+++ app/admin/venue.rb
@@ -13,24 +13,30 @@ ActiveAdmin.register Venue do
 
   show do
     h3 venue.location
-      div do
-        simple_format venue.street
-        simple_format venue.zipcode
-        simple_format venue.city
-        simple_format venue.url
-      end
-      attributes_table do
+    div do
+      venue.street
+    end
+    div do
+      venue.zipcode + " " + venue.city
+    end
+    div do
+      "<a href='#{venue.url}'>#{venue.url}</a>".html_safe
+    end
+    div do
+      "<a class='map-link' href='http://maps.google.com/maps?z=18&q=#{venue.latitude},#{venue.longitude}' data-lat='#{venue.latitude}' data-lng='#{venue.longitude}'>Google Maps</a>".html_safe
+    end
+    attributes_table do
       row :single_events do |preset|
         ul do
           venue.single_events.map { |s| li(link_to(s.full_name, edit_admin_event_single_event_path(s.event, s))) }
-          end
         end
+      end
       row :events do |preset|
         ul do
           venue.events.map { |s| li(link_to(s.name, edit_admin_event_path(s))) }
-          end
         end
       end
+    end
   end
 
   form do
