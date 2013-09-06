2fe4134dfe3be3ba50efa09cd6c8990c2adbf728
moonglum <moonglum@moonbeamlabs.com>
2013-09-06 13:45:05 +0200

    Looks like someone didn't check in the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -9,32 +9,32 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130914114328) do
+ActiveRecord::Schema.define(version: 20130728123334) do
 
-  create_table "authorizations", :force => true do |t|
+  create_table "authorizations", force: true do |t|
     t.string   "provider"
     t.string   "uid"
     t.integer  "user_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "token"
     t.string   "secret"
     t.datetime "token_expires"
     t.string   "temp_token"
   end
 
-  add_index "authorizations", ["user_id"], :name => "index_authorizations_on_user_id"
+  add_index "authorizations", ["user_id"], name: "index_authorizations_on_user_id", using: :btree
 
-  create_table "categories", :force => true do |t|
+  create_table "categories", force: true do |t|
     t.string   "title"
     t.string   "color"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "comments", :force => true do |t|
+  create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
     t.integer  "commentable_id"
@@ -43,20 +43,20 @@ ActiveRecord::Schema.define(:version => 20130914114328) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable_id_and_commentable_type"
-  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
-  create_table "event_curations", :force => true do |t|
+  create_table "event_curations", force: true do |t|
     t.integer  "user_id"
     t.integer  "event_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "event_curations", ["event_id"], :name => "index_event_curations_on_event_id"
-  add_index "event_curations", ["user_id"], :name => "index_event_curations_on_user_id"
+  add_index "event_curations", ["event_id"], name: "index_events_users_on_event_id", using: :btree
+  add_index "event_curations", ["user_id"], name: "index_events_users_on_user_id", using: :btree
 
-  create_table "events", :force => true do |t|
+  create_table "events", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.text     "schedule_yaml"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(:version => 20130914114328) do
     t.string   "twitter"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "full_day",        :default => false
+    t.boolean  "full_day",        default: false
     t.string   "twitter_hashtag"
     t.integer  "category_id"
     t.integer  "venue_id"
@@ -73,53 +73,52 @@ ActiveRecord::Schema.define(:version => 20130914114328) do
     t.integer  "region_id"
   end
 
-  add_index "events", ["category_id"], :name => "index_events_on_category_id"
-  add_index "events", ["picture_id"], :name => "index_events_on_picture_id"
-  add_index "events", ["region_id"], :name => "index_events_on_region_id"
-  add_index "events", ["venue_id"], :name => "index_events_on_venue_id"
+  add_index "events", ["category_id"], name: "index_events_on_category_id", using: :btree
+  add_index "events", ["picture_id"], name: "index_events_on_picture_id", using: :btree
+  add_index "events", ["region_id"], name: "index_events_on_region_id", using: :btree
+  add_index "events", ["venue_id"], name: "index_events_on_venue_id", using: :btree
 
-  create_table "pictures", :force => true do |t|
+  create_table "pictures", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "box_image"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "region_organizers", :force => true do |t|
+  create_table "region_organizers", force: true do |t|
     t.integer  "region_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "region_organizers", ["region_id"], :name => "index_region_organizers_on_region_id"
-  add_index "region_organizers", ["user_id"], :name => "index_region_organizers_on_user_id"
+  add_index "region_organizers", ["region_id"], name: "index_regions_users_on_region_id", using: :btree
+  add_index "region_organizers", ["user_id"], name: "index_regions_users_on_user_id", using: :btree
 
-  create_table "regions", :force => true do |t|
+  create_table "regions", force: true do |t|
     t.string   "name"
     t.string   "slug"
     t.float    "latitude"
     t.float    "longitude"
-    t.float    "perimeter",  :default => 20.0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.float    "perimeter",  default: 20.0
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  add_index "regions", ["slug"], :name => "index_regions_on_slug"
+  add_index "regions", ["slug"], name: "index_regions_on_slug", using: :btree
 
-  create_table "single_event_external_users", :force => true do |t|
+  create_table "single_event_external_users", force: true do |t|
     t.integer  "single_event_id"
     t.string   "email"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "session_token"
   end
 
-  add_index "single_event_external_users", ["single_event_id"], :name => "index_single_event_external_users_on_single_event_id"
+  add_index "single_event_external_users", ["single_event_id"], name: "index_single_event_external_users_on_single_event_id", using: :btree
 
-  create_table "single_events", :force => true do |t|
+  create_table "single_events", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "event_id"
@@ -130,38 +129,40 @@ ActiveRecord::Schema.define(:version => 20130914114328) do
     t.integer  "duration"
     t.boolean  "full_day"
     t.string   "twitter_hashtag"
-    t.boolean  "based_on_rule",           :default => false
+    t.boolean  "based_on_rule",           default: false
     t.integer  "category_id"
     t.integer  "venue_id"
     t.string   "venue_info"
     t.integer  "picture_id"
     t.string   "twitter"
-    t.boolean  "use_venue_info_of_event", :default => true
-    t.integer  "region_id"
+    t.boolean  "use_venue_info_of_event", default: true
   end
 
-  add_index "single_events", ["category_id"], :name => "index_single_events_on_category_id"
-  add_index "single_events", ["event_id"], :name => "index_single_events_on_event_id"
-  add_index "single_events", ["picture_id"], :name => "index_single_events_on_picture_id"
-  add_index "single_events", ["region_id"], :name => "index_single_events_on_region_id"
-  add_index "single_events", ["venue_id"], :name => "index_single_events_on_venue_id"
+  add_index "single_events", ["category_id"], name: "index_single_events_on_category_id", using: :btree
+  add_index "single_events", ["event_id"], name: "index_single_events_on_event_id", using: :btree
+  add_index "single_events", ["picture_id"], name: "index_single_events_on_picture_id", using: :btree
+  add_index "single_events", ["venue_id"], name: "index_single_events_on_venue_id", using: :btree
 
-  create_table "single_events_users", :id => false, :force => true do |t|
+  create_table "single_events_users", id: false, force: true do |t|
     t.integer "user_id"
     t.integer "single_event_id"
   end
 
-  add_index "single_events_users", ["single_event_id"], :name => "index_single_events_users_on_single_event_id"
-  add_index "single_events_users", ["user_id"], :name => "index_single_events_users_on_user_id"
+  add_index "single_events_users", ["single_event_id"], name: "index_single_events_users_on_single_event_id", using: :btree
+  add_index "single_events_users", ["user_id"], name: "index_single_events_users_on_user_id", using: :btree
 
-  create_table "suggestions", :force => true do |t|
+  create_table "suggestions", force: true do |t|
+    t.string   "name"
+    t.string   "occurrence"
     t.text     "description"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.text     "place"
+    t.text     "more"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "email_address"
   end
 
-  create_table "taggings", :force => true do |t|
+  create_table "taggings", force: true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
     t.string   "taggable_type"
@@ -171,31 +172,32 @@ ActiveRecord::Schema.define(:version => 20130914114328) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-  add_index "taggings", ["tagger_id", "tagger_type"], :name => "index_taggings_on_tagger_id_and_tagger_type"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+  add_index "taggings", ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type", using: :btree
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: true do |t|
     t.string  "name"
     t.integer "category_id"
   end
 
-  add_index "tags", ["category_id"], :name => "index_tags_on_category_id"
+  add_index "tags", ["category_id"], name: "index_tags_on_category_id", using: :btree
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "email"
     t.string   "encrypted_password"
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  :default => false
-    t.string   "nickname",               :default => "",    :null => false
+    t.boolean  "admin",                  default: false
+    t.string   "nickname",               default: "",    null: false
+    t.integer  "current_region_id",      default: 2
     t.text     "description"
     t.string   "github"
     t.string   "twitter"
@@ -210,25 +212,26 @@ ActiveRecord::Schema.define(:version => 20130914114328) do
     t.string   "gravatar_email"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["nickname"], :name => "index_users_on_nickname", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "venues", :force => true do |t|
+  create_table "venues", force: true do |t|
     t.string   "location"
+    t.integer  "region_id",  default: 2
     t.string   "street"
     t.string   "zipcode"
     t.string   "city"
     t.string   "country"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "url"
     t.integer  "region_id",  :default => 2
   end
 
-  add_index "venues", ["latitude", "longitude"], :name => "index_venues_on_latitude_and_longitude"
-  add_index "venues", ["region_id"], :name => "index_venues_on_region_id"
+  add_index "venues", ["latitude", "longitude"], name: "index_venues_on_latitude_and_longitude", using: :btree
+  add_index "venues", ["region_id"], name: "index_venues_on_region_id", using: :btree
 
 end
