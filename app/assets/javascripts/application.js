b02515cba0c87b8f1812ad7b9a3a03bac378dd21
Bodo Tasche <bodo@wannawork.de>
2013-11-25 19:44:53 +0100

    fixed asset pipeline. require block needs to be at the beginning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,9 +7,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require leaflet
-
-L.Icon.Default.imagePath = "/assets/";
-
 //= require jquery.balloon
 //= require jquery.tokeninput
 //= require jquery.endless-scroll
@@ -19,3 +16,6 @@ L.Icon.Default.imagePath = "/assets/";
 //= require jquery.pjax
 //= require_tree .
 //= require twitter/bootstrap
+
+L.Icon.Default.imagePath = "/assets/";
+
