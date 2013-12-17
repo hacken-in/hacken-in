19c361e9aab9733568a919b3a2d58e153531af53
moonglum <moonglum@moonbeamlabs.com>
2013-12-17 12:03:34 +0100

    Require Foundation JS parts explicitly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,23 @@
 //= require jquery
 //= require jquery_ujs
-//= require foundation
+
+// TODO: Don't include the parts we don't need
+// Please only remove the equal sign to make insertion easier
+//= require foundation/foundation
+//= require foundation/foundation.abide
+//= require foundation/foundation.accordion
+//= require foundation/foundation.alert
+//= require foundation/foundation.clearing
+//= require foundation/foundation.dropdown
+//= require foundation/foundation.interchange
+//= require foundation/foundation.joyride
+//= require foundation/foundation.magellan
+//= require foundation/foundation.offcanvas
+//= require foundation/foundation.orbit
+//= require foundation/foundation.reveal
+//= require foundation/foundation.tab
+//= require foundation/foundation.tooltip
+//= require foundation/foundation.topbar
 
 //= require_tree .
 
