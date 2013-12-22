e6f3ae0f20c8f3dfcba6060293f56b5065cc3a00
moonglum <moonglum@moonbeamlabs.com>
2013-12-22 16:56:39 +0100

    Calendar Refactoring

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,25 +1,32 @@
+require 'forwardable'
 require 'day'
 
 # Filter, sort and group a list of single events
 # (It is not aware that something like a database or Rails exists)
 class Calendar
+  extend Forwardable
+
+  # Does the calendar contain no entries?
+  def_delegator :days, :empty?
+
   # Create a new instance given an event list and a user
   # The third argument is the class for creating the days
-  def initialize(event_list, user, day_class = Day)
+  def initialize(events, user, day_class = Day)
     @user = user
-    @event_list = event_list.select { |event| event.is_for_user? @user }
+    @events = events
     @day_class = day_class
   end
 
   # Yield the days in the right order
   def each
-    @event_list.group_by { |event| event.date }.sort.each do |date, events|
+    days.sort.each do |date, events|
       yield @day_class.new(date, events)
     end
   end
 
-  # Does the calendar contain no entries?
-  def empty?
-    @event_list.empty?
+  private
+
+  def days
+    @days ||= @events.select { |event| event.is_for_user? @user }.group_by { |event| event.date }
   end
 end
