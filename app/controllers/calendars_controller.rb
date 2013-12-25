9f737d92ee65e69b4cd7d1f27f75114898743680
moonglum <moonglum@moonbeamlabs.com>
2013-12-25 20:10:56 +0100

    CalendarsController: Simplify start_date determination
    This was a little bit too much. Make it simple with an inline
    rescue clause. Whatever goes wrong in the parsing of the param
    (whether it is because the param is empty or not parsable)
    default to today.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,17 +4,8 @@ class CalendarsController < ApplicationController
   def show
     raise ActionController::RoutingError.new('Not Found') if current_region.nil?
 
+    start_date      = Date.parse(params[:start]) rescue Date.today
     @start_selector = StartSelector.new(start_date)
     @calendar       = Calendar.new(start_date, current_region, current_user)
-  end
-
-  private
-
-  def start_date
-    @start_date ||= begin
-      Date.parse(params[:start])
-    rescue ArgumentError, TypeError
-      Date.today
-    end
   end
 end
