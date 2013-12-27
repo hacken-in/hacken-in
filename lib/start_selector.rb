37d33eb7157e8d6335e1c74caadd41744aa7aeab
moonglum <moonglum@moonbeamlabs.com>
2013-12-27 22:35:12 +0100

    Day and Month selector is working on small screens now
    Thanks @EinLama

--- lib/start_selector.rb
+++ lib/start_selector.rb
@@ -1,19 +1,19 @@
 class StartSelector
-  NUMBER_OF_MONTHS = 8
-  NUMBER_OF_DAYS = 5
+  NUMBER_OF_MONTHS = 20
+  NUMBER_OF_DAYS = 20
 
   def initialize(start_date)
     @start_date = start_date
   end
 
   def months
-    months = (0..NUMBER_OF_MONTHS).map { |i| MonthPresenter.new(@start_date + i.months) }
+    months = (0...NUMBER_OF_MONTHS).map { |i| MonthPresenter.new(@start_date + i.months) }
     months.first.active = true
     months
   end
 
   def days
-    date_range = @start_date .. @start_date + NUMBER_OF_DAYS.days
+    date_range = @start_date ... @start_date + NUMBER_OF_DAYS.days
 
     days = SingleEvent.events_per_day_in(date_range).sort.map do |day, occurrences|
       DayPresenter.new(day, (occurrences > 0))
