fa692b8b6575285b8a42f58c3ac57215a70247ee
moonglum <moonglum@moonbeamlabs.com>
2013-12-28 22:03:43 +0100

    Fixup CalendarsController: Simplify start_date determination
    Calendars Controller refactored with @railsbros-dirk
    Better Solution

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,8 +4,13 @@ class CalendarsController < ApplicationController
   def show
     raise ActionController::RoutingError.new('Not Found') if current_region.nil?
 
-    start_date      = Date.parse(params[:start]) rescue Date.today
     @start_selector = StartSelector.new(start_date)
     @calendar       = Calendar.new(start_date, current_region, current_user)
+  end
+
+  private
+
+  def start_date
+    @start_date ||= params[:start].present? ? Date.parse(params[:start]) : Date.today
   end
 end
