1416a2d5e678005f8d96a35ceca34b2985a724f3
moonglum <moonglum@moonbeamlabs.com>
2014-01-04 11:29:02 +0100

    Removed unnecessary check
    Why would anyone change a comment without changing anything?

--- lib/user_edit_observer.rb
+++ lib/user_edit_observer.rb
@@ -2,10 +2,7 @@ class UserEditObserver < ActiveRecord::Observer
   observe :comment
 
   def after_update(record)
-    changes = record.changes.except(:updated_at)
-    if changes.keys.count > 0
-      ChangeMailer.mail_changes(record, changes).deliver
-    end
+    ChangeMailer.mail_changes(record, record.changes)
   end
 
   def after_create(record)
