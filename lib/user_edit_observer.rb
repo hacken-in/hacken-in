e5d51ed13e09ec8297e19703c0b2af910c3021db
moonglum <moonglum@moonbeamlabs.com>
2014-01-04 11:17:12 +0100

    Move after_save for Comments to the Comment itself
    This was just evil

--- lib/user_edit_observer.rb
+++ lib/user_edit_observer.rb
@@ -1,12 +1,6 @@
 class UserEditObserver < ActiveRecord::Observer
   observe :comment
 
-  def before_save(record)
-    if !record.new_record?
-      record.instance_variable_set(:@user_edit_observer_old_state, record.class.find(record.id).to_json)
-    end
-  end
-
   def after_update(record)
     changes = record.changes.except(:updated_at)
     if changes.keys.count > 0
