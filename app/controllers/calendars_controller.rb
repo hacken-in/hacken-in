3c9aa011f815a9fe7e81b3efddfacf0566da21cb
moonglum <moonglum@moonbeamlabs.com>
2014-01-17 10:07:58 +0100

    Move checking for the route to a before_filter

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,9 +1,8 @@
 #encoding: utf-8
 class CalendarsController < ApplicationController
+  before_action :require_region!, only: [ :show ]
 
   def show
-    raise ActionController::RoutingError.new('Not Found') if current_region.nil?
-
     @start_selector = StartSelector.new(start_date)
     @calendar       = Calendar.new(start_date, current_region, current_user)
   end
@@ -12,5 +11,10 @@ class CalendarsController < ApplicationController
 
   def start_date
     @start_date ||= params[:start].present? ? Date.parse(params[:start]) : Date.today
+  end
+
+  # Raise a Not Found Routing Exception if no region was set
+  def require_region!
+    raise ActionController::RoutingError.new('Not Found') if current_region.nil?
   end
 end
