17148e7833381bc619acc30f29472912e9547906
moonglum <moonglum@moonbeamlabs.com>
2014-01-17 10:11:31 +0100

    The method to determine the start date is not necessary

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,13 +3,9 @@ class CalendarsController < ApplicationController
   before_action :require_region!, only: [ :show ]
 
   def show
+    start_date = params[:start].present? ? Date.parse(params[:start]) : Date.today
+
     @start_selector = StartSelector.new(start_date)
     @calendar       = Calendar.new(start_date, current_region, current_user)
-  end
-
-  private
-
-  def start_date
-    @start_date ||= params[:start].present? ? Date.parse(params[:start]) : Date.today
   end
 end
