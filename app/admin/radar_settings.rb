4801450eef56a777b6310c51495e097e80c29f91
Bodo Tasche <bodo@wannawork.de>
2014-03-01 22:26:17 +0100

    added radar settings to the admin view

---
+++ app/admin/radar_settings.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register RadarSetting do
+
+  menu false
+  controller do
+    nested_belongs_to :event
+
+    def destroy
+      destroy!{ admin_event_path(params[:event_id]) }
+    end
+
+    def create
+      create!{ admin_event_path(params[:event_id]) }
+    end
+
+    def permitted_params
+      params.permit(radar_setting: [
+        :event_id,
+        :url,
+        :radar_type
+      ])
+    end
+  end
+
+  form do |f|
+    f.inputs "Radar Einstellung" do
+      f.input :url
+      f.input :radar_type, as: :select, collection: ["Meetup"]
+    end
+  end
+
+end
