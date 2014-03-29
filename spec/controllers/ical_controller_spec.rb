9b75ee8568b9e32898186d73c73547cc53818a10
Bodo Tasche <bodo@wannawork.de>
2014-03-29 18:04:17 +0100

    okay, outdated example...this one should work

--- spec/controllers/ical_controller_spec.rb
+++ spec/controllers/ical_controller_spec.rb
@@ -23,7 +23,7 @@ def generate_event_entry(single_event, description = "")
   event += "URL:http://hacken.dev/events/#{single_event.event.id}/dates/#{single_event.id}\r\n"
   event += "END:VEVENT\r\n"
   event += "(.*)"
-  Regexp.new(event, "m")
+  Regexp.new(event, Regexp::MULTILINE)
 end
 
 describe IcalController do
