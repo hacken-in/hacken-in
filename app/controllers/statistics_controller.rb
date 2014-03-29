f549cc28d00753e01e05abf4898c7aa53a505e03
Bodo Tasche <bodo@wannawork.de>
2014-03-29 13:51:08 +0100

    haha, mysql starts weekdays with 1 :clap:

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -7,13 +7,11 @@ class StatisticsController < ApplicationController
   def show
     week_data = SingleEvent.in_region(@current_region).where("occurrence <= ?", Date.yesterday).group("weekday(occurrence)").count
 
-    day_names = I18n.t(:"date.day_names")
+    day_names = I18n.t(:"date.day_names").rotate
 
     @chart_data = week_data.keys.sort.map do |key|
       [day_names[key], week_data[key]]
     end
-    # Sunday => Last day
-    @chart_data.rotate
   end
 
 end
