ffa72fc1057a8b14536bbb19c6b8ae51cfe07b37
moonglum <moonglum@moonbeamlabs.com>
2014-06-11 22:30:48 +0200

    Kaminari is used strictly presentational here, push it to the presenter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
     unless params[:search].blank?
-      single_events = Kaminari.paginate_array(SingleEvent.in_region(current_region).search(params[:search])).page(params[:page]).per(10)
-      @search_result = SearchResult.new(single_events)
+      single_events = SingleEvent.in_region(current_region).search(params[:search])
+      @search_result = SearchResult.new(single_events, params[:page])
     end
   end
 end
--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,7 +1,9 @@
 class SearchResult
   attr_reader :entries
+  PER_PAGE = 10
 
-  def initialize(single_events, entry_class = CalendarEntry)
+  def initialize(single_events, page, entry_class = CalendarEntry)
+    single_events = Kaminari.paginate_array(single_events).page(page).per(PER_PAGE)
     @entries = single_events.map { |event| entry_class.new(event) }
   end
 end
