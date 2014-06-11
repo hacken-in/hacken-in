64f40381a3eed6aa586655ca2241663ed50074d0
Bodo Tasche <bodo@wannawork.de>
2014-06-11 20:52:39 +0200

    removed duplicate code

--- app/controllers/api/calendars_controller.rb
+++ app/controllers/api/calendars_controller.rb
@@ -3,22 +3,21 @@ class Api::CalendarsController < ApplicationController
   layout false
 
   def entries
-    start_date = if params[:start].present?
-                   Date.parse(params[:start])
-                 else
-                   Date.today
-                 end
-
     @calendar = Calendar.new(start_date, current_region, current_user)
   end
 
   def selector
-    start_date = if params[:start].present?
-                   Date.parse(params[:start])
-                 else
-                   Date.today
-                 end
     @start_selector = StartSelector.new(start_date)
+  end
+
+  private
+
+  def start_date
+    if params[:start].present?
+      Date.parse(params[:start])
+    else
+      Date.today
+    end
   end
 
 end
