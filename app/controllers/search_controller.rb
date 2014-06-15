ede220b6d21ab5bee3a481c2a1ccadbd30f955ce
Lucas Dohmen <lucas@dohmen.io>
2014-06-15 17:24:12 +0200

    Refactor Search Controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,8 @@
 class SearchController < ApplicationController
   def index
-    if params[:search].blank?
-      redirect_to region_path(current_region) 
-    else
-      single_events = SingleEvent.search_in_region(params[:search], current_region)
-      @search_result = SearchResult.new(single_events, page)
-    end
+    redirect_to(region_path(current_region)) && return if params[:search].blank?
+    single_events = SingleEvent.search_in_region(params[:search], current_region)
+    @search_result = SearchResult.new(single_events, page)
   end
 
   private
