5c403561a55e819cf4453ac1155d43a545676c8e
Sebastian Schulze <github.com@bascht.com>
2014-12-29 14:20:44 +0100

    Migrate existing Cronjobs to Whenever
    (closes #452)

---
+++ config/schedule.rb
@@ -0,0 +1,22 @@
+# Shellycloud will *only* pick up this file
+set :output, 'whenever.log'
+
+every :day, :at => '03:21am' do
+  rake 'twitter:follow'
+end
+
+every :day, :at => '05:21am' do
+  rake 'twitter:update_lists'
+end
+
+every :day, :at => '05:41am' do
+  rake 'radar:fetch'
+end
+
+every :day, :at => '06:41pm' do
+  rake 'radar:fetch'
+end
+
+every :day, :at => '00:30am' do
+  rake 'single_events:generate'
+end
