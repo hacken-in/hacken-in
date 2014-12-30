c2785c580000eb9225d533c8293df44b69aa2b01
Sebastian Schulze <github.com@bascht.com>
2014-12-30 14:56:12 +0100

    Activate Bugsnag for master environment as well (fixes #506)

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,9 @@
 if Rails.application.secrets.key? :bugsnag_api_key
   Bugsnag.configure do |config|
+    # See https://bugsnag.com/docs/notifiers/ruby#configuration
+    # for all available configuration keys
     config.api_key = Rails.application.secrets.bugsnag_api_key
+
+    config.release_stage = ENV['CLOUD_NAME'] == 'hacken-in-master' ? 'master' : 'production'
   end
 end
