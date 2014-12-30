4dd1491af4509c888438d66f3b1120af2a97db19
Bodo Tasche <bodo@wannawork.de>
2014-12-30 11:12:11 +0100

    added password email address, fixes #497

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "mail@yourhost.de"
+  config.mailer_sender = "mail@hacken.in"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
