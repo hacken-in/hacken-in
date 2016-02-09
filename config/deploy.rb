6f4ab01fe63433afa714cc1b848ed777edae0f5c
Sebastian Schulze <github.com@bascht.com>
2016-02-09 10:05:34 +0100

    Restart Unicorn on every deployment. Shall we?

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,11 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 
 namespace :deploy do
-  after :restart, :clear_cache do
+  task :restart do
     on roles(:web) do
       run "svc -h svc -h ~/service/hacken-in-#{fetch(:stage)}"
     end
   end
 end
+
+after 'deploy:publishing', 'deploy:restart'
