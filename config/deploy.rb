7863efff6d9488d85b8586ebca9155701349d164
Sebastian Schulze <github.com@bascht.com>
2016-03-25 17:41:46 +0100

    Explicitly set roles + ident for Whenever  (#616)

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :user, 'hacken'
 set :log_level, :info
 
 set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads']
+set :whenever_roles, ->{ :app }
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 set :keep_releases, 5
 
