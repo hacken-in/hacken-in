05fb6dd52f9bf3ed2e58a05475b5d786fe7adc18
Sebastian Schulze <github.com@bascht.com>
2016-03-25 15:10:08 +0100

    Brute force symlinks (refs #616)

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,16 @@ set :repo_url, 'https://github.com/hacken-in/website.git'
 
 set :user, 'hacken'
 set :log_level, :info
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads']
 
 set :keep_releases, 5
 
 namespace :deploy do
   task :restart do
-    execute "svc -h svc -h ~/service/hacken-in-#{fetch(:stage)}"
+    on roles(:app) do
+      execute "svc -h svc -h ~/service/hacken-in-#{fetch(:stage)}"
+    end
   end
 end
 
